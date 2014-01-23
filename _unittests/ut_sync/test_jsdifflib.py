5205d3617cd08f20a6d599469dc36e37b915ffb2
sdpython <xavier.dupre@gmail.com>
2014-01-24 00:54:50 +0100

    adding a function to quickly visualize differences between two files

---
+++ _unittests/ut_sync/test_jsdifflib.py
@@ -0,0 +1,42 @@
+"""
+@brief      test tree node (time=5s)
+"""
+
+
+from __future__ import print_function
+import sys, os, unittest, re, io
+
+try :
+    import src
+except ImportError :
+    path = os.path.normpath(os.path.abspath( os.path.join( os.path.split(__file__)[0], "..", "..")))
+    if path not in sys.path : sys.path.append (path)
+    import src
+
+try :
+    import pymyinstall
+except ImportError :
+    path = os.path.normpath(os.path.abspath( os.path.join( os.path.split(__file__)[0], "..", "..", "..", "pymyinstall", "src")))
+    if path not in sys.path : sys.path.append (path)
+    import pymyinstall
+
+from src.pyquickhelper.loghelper.flog        import fLOG
+from src.pyquickhelper.sync.visual_sync      import create_visual_diff_through_html
+
+class TestJsDiffLib(unittest.TestCase):
+    
+    def test_jsdifflib(self) :
+        fLOG (__file__, self._testMethodName, OutputPrint = __name__ == "__main__")
+        tt = os.path.split(src.pyquickhelper.sync.visual_sync.__file__)[0]
+        ma = os.path.join(tt, "jsdifflib-master")
+        if os.path.exists(ma):
+            for i in os.listdir(ma) :
+                os.remove(os.path.join(ma,i))
+        assert not os.path.exists(os.path.join(ma,"difflib.js"))
+        p = create_visual_diff_through_html("a","b")
+        assert len(p) > 0
+        assert os.path.exists(os.path.join(ma,"difflib.js"))
+        
+
+if __name__ == "__main__"  :
+    unittest.main ()    
---
+++ src/pyquickhelper/sync/visual_sync.py
@@ -0,0 +1,149 @@
+# coding: latin-1
+"""
+@file
+@brief Quick and dirty trick to use `jsdifflib <https://github.com/cemerick/jsdifflib>`_
+to visualize the differences between two files.
+"""
+
+import sys, os, re, zipfile, datetime, gzip
+
+html_page = """
+    <!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+        <title>jsdifflib demo</title>
+        <link rel="stylesheet" type="text/css" href="__PATH__diffview.css"/>
+        <script type="text/javascript" src="__PATH__diffview.js"></script>
+        <script type="text/javascript" src="__PATH__difflib.js"></script>
+    <style type="text/css">
+    body {
+        font-size: 12px;
+        font-family: Sans-Serif;
+    }
+    h2 {
+        margin: 0.5em 0 0.1em;
+        text-align: center;
+    }
+    .top {
+        text-align: center;
+    }
+    .textInput {
+        display: block;
+        width: 49%;
+        float: left;
+    }
+    textarea {
+        width:100%;
+        height:300px;
+    }
+    label:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+    .spacer {
+        margin-left: 10px;
+    }
+    .viewType {
+        font-size: 16px;
+        clear: both;
+        text-align: center;
+        padding: 1em;
+    }
+    #diffoutput {
+        width: 100%;
+    }
+    </style>
+
+    <script type="text/javascript">
+
+    function diffUsingJS(viewType) {
+        "use strict";
+        var byId = function (id) { return document.getElementById(id); },
+            base = difflib.stringAsLines(byId("baseText").value),
+            newtxt = difflib.stringAsLines(byId("newText").value),
+            sm = new difflib.SequenceMatcher(base, newtxt),
+            opcodes = sm.get_opcodes(),
+            diffoutputdiv = byId("diffoutput"),
+            contextSize = byId("contextSize").value;
+
+        diffoutputdiv.innerHTML = "";
+        contextSize = contextSize || null;
+
+        diffoutputdiv.appendChild(diffview.buildView({
+            baseTextLines: base,
+            newTextLines: newtxt,
+            opcodes: opcodes,
+            baseTextName: "Base Text",
+            newTextName: "New Text",
+            contextSize: contextSize,
+            viewType: viewType
+        }));
+    }
+
+    </script>
+    </head>
+    <body>
+        <h1 class="top"><a href="http://github.com/cemerick/jsdifflib">jsdifflib</a> demo</h1>
+        <div class="top">
+            <strong>Context size (optional):</strong> <input type="text" id="contextSize" value="" />
+        </div>
+        <div class="textInput">
+            <h2>Base Text</h2>
+            <textarea id="baseText">__STRING1__</textarea>
+        </div>
+        <div class="textInput spacer">
+            <h2>New Text</h2>
+            <textarea id="newText">__STRING2__</textarea>
+        </div>
+        <div class="viewType">
+            <input type="radio" name="_viewtype" id="sidebyside" onclick="diffUsingJS(0);" /> <label for="sidebyside">Side by Side Diff</label>
+            &nbsp; &nbsp;
+            <input type="radio" name="_viewtype" id="inline" onclick="diffUsingJS(1);" /> <label for="inline">Inline Diff</label>
+        </div>
+        <div id="diffoutput"> </div>
+    </body>
+    </html>
+    """
+
+def create_visual_diff_through_html(string1, string2):
+    """
+    The function uses `jsdifflib <https://github.com/cemerick/jsdifflib>`_
+    to create a visual diff.
+    If it was not already done, the function downloads
+    the tool using 
+    `pymyinstall <https://github.com/sdpython/pymyinstall>`_.
+    
+    @param  string1         first string
+    @param  string2         second string
+    @return                 html page
+    
+    Example:
+    @code
+    while open("file1.txt","r",encoding="utf8") as f : text1 = f.read()
+    while open("file2.txt","r",encoding="utf8") as f : text2 = f.read()
+    pg = create_visual_diff_through_html(text1,text2)
+    with open("page.html","w",encoding="utf8") as f : f.write(pg)
+    import webbrowser
+    webbrowser.open("page.html")
+    @endcode
+    """
+    if True :
+        from pymyinstall import ModuleInstall
+        temp = os.path.abspath(os.path.split(__file__)[0])
+        ModuleInstall("jsdifflib","github",gitrepo="cemerick", fLOG = lambda *s : None).download(temp_folder = temp)
+    
+    fold = os.path.abspath(os.path.join(os.path.split(__file__)[0],"jsdifflib-master"))
+    if not os.path.exists(fold):
+        raise FileNotFoundError("unable to find jsdifflib in: " + fold)
+    global html_page
+    
+    def cleanh(s) :
+        return s.replace("&","&amp;").replace("<","&lt;").replace(">","&gt;")
+    
+    page = html_page.replace("__PATH__",fold + "\\") \
+                    .replace("__STRI"+"NG1__", cleanh(string1)) \
+                    .replace("__STRI"+"NG2__", cleanh(string2))
+    
+    return page
