d47cc56b874c9a8c3f65a76a02c4975dc7a23c22
sdpython <xavier.dupre@gmail.com>
2014-02-26 01:33:16 +0100

    version modification in the setup

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ if "--verbose" in sys.argv :
 
 setup(
     name                    = project_var_name,
-    version                 = 'py%s-v%s.%s' % (versionPython, sversion, subversion),
+    version                 = '%s.%s' %(sversion, subversion) if "register" in sys.argv else 'py%s-v%s.%s' % (versionPython, sversion, subversion),
     author                  = 'Xavier Dupré',
     author_email            = 'xavier.dupre AT gmail.com',
     url                     = "http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/index.html",
