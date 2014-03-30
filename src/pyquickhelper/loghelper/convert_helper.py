91f8b43b9497287547f7b219fc674e576f9b0c99
sdpython <xavier.dupre@gmail.com>
2014-03-30 16:53:25 +0200

    fix an issue while converting strings to datetime (with symbol -)

--- src/pyquickhelper/loghelper/convert_helper.py
+++ src/pyquickhelper/loghelper/convert_helper.py
@@ -19,6 +19,7 @@ def str_to_datetime (dt, format = None):
     @rtype              datetime
     """
     if "+" in dt : dt = dt.split("+")[0].strip()
+    elif " -" in dt : dt = dt.split(" -")[0].strip()
     if format == None :
         if "." in dt :        
             return datetime.datetime.strptime (dt, "%Y-%m-%d %H:%M:%S.%f")    
