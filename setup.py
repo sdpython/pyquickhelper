910265e51b8ff6476686aa612345334924a798ee
sdpython <xavier.dupre@gmail.com>
2014-04-06 11:33:11 +0200

    update version + read version from setup.py for documentation

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ else :
     subversion = 1   
 
 project_var_name    = "pyquickhelper"
-sversion            = "0.3"
+sversion            = "0.4"
 versionPython       = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
 path                = "Lib/site-packages/" + project_var_name
 readme              = 'README.rst'
