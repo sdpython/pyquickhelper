b33581b49666257d964a1dc3a2eba0ff0d32f044
sdpython <xavier.dupre@gmail.com>
2014-07-02 01:22:56 +0200

    move unecessary lines

--- src/pyquickhelper/helpgen/utils_pywin32.py
+++ src/pyquickhelper/helpgen/utils_pywin32.py
@@ -16,12 +16,12 @@ def import_pywin32():
         if "DLL load failed:" in str(e):
             import os,sys
             path = os.path.join(os.path.split(sys.executable)[0], "Lib","site-packages","pywin32_system32")
-            dll = os.listdir(path)
-            dll = [os.path.join(path,_) for _ in dll if "dll" in _]
             os.environ["PATH"] = os.environ["PATH"] + ";" + path
             try:
                 import win32com
             except ImportError as ee :
+                dll = os.listdir(path)
+                dll = [os.path.join(path,_) for _ in dll if "dll" in _]
                 raise ImportError("some DLL must be copied:\n" + "\n".join(dll)) from e
         else :
             raise e
