72c23b7d60ef870f045be1e441fc5648efee24ef
sdpython <xavier.dupre@gmail.com>
2014-07-19 00:16:34 +0200

    check the setup installation

--- build_setup_help_on_windows.bat
+++ build_setup_help_on_windows.bat
@@ -4,19 +4,35 @@ del /Q dist\*.*
 
 rem unittests with python 3.4
 
+IF EXIST c:\Python34vir GOTO next:
+mkdir c:\Python34vir
+
+:next:
+IF EXIST c:\Python34vir\install GOTO fullsetup:
+c:\Python34\Scripts\virtualenv c:\Python34vir\install --system-site-packages
+
+:fullsetup:
+
+echo #######################################################
+c:\Python34vir\install\Scripts\python -u setup.py install
+echo #######################################################
+
 set pythonexe="c:\Python34\python"
 %pythonexe% -u setup.py unittests
+echo #######################################################
 
 rem python 3.3
 
 set pythonexe="c:\Python33_x64\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
+echo #######################################################
 
 set pythonexe="c:\Python33\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py sdist --formats=gztar,zip --verbose
 %pythonexe% setup.py bdist_wininst
+echo #######################################################
 
 rem python 3.4
 
@@ -24,16 +40,19 @@ set pythonexe="c:\Python34_x64\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
 %pythonexe% setup.py build bdist_msi --plat-name=win-amd64
+echo #######################################################
 
 set pythonexe="c:\Python34\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py sdist --formats=gztar,zip --verbose
 %pythonexe% setup.py bdist_wininst
 %pythonexe% setup.py bdist_msi
+echo #######################################################
 
 rem help
 
 %pythonexe% -u setup.py build_sphinx
+echo #######################################################
 
 if not exist dist\html mkdir dist\html
 xcopy /E /C /I /Y _doc\sphinxdoc\build\html dist\html
