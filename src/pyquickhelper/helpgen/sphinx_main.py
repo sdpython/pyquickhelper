32b594567cb704b167e21b7a81fec244526112d1
sdpython <xavier.dupre@gmail.com>
2014-08-24 15:39:02 +0200

    second fix for the previous bug

--- src/pyquickhelper/helpgen/sphinx_main.py
+++ src/pyquickhelper/helpgen/sphinx_main.py
@@ -892,7 +892,12 @@ def post_process_latex(st, doall):
         
     st = st.replace("\\chapter", "\\section")
     st = st.replace("\\newchapter", "\\chapter")
-    st = st.replace(r"\usepackage{multirow}", r"\usepackage{multirow}\usepackage{amssymb}\usepackage{latexsym}\usepackage{amsfonts}\usepackage{ulem}")
+    if r"\usepackage{multirow}" in st :
+        st = st.replace(r"\usepackage{multirow}", r"\usepackage{multirow}\usepackage{amssymb}\usepackage{latexsym}\usepackage{amsfonts}\usepackage{ulem}")
+    elif r"\usepackage{hyperref}" in st :
+        st = st.replace(r"\usepackage{hyperref}", r"\usepackage{hyperref}\usepackage{amssymb}\usepackage{latexsym}\usepackage{amsfonts}\usepackage{ulem}")
+    else :
+        raise HelpGenException("unable to add new instructions usepackage")
     
     return st
     
