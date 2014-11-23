77ce7701890ac52719ff751235d3382db9ea34bf
azure provisioned user <pyxavierdupre@ubuntu12python.ubuntu12python.a4.internal.cloudapp.net>
2014-11-23 14:13:16 +0000

    update pathfor the notebooks and documentation (linux)

--- open_ipython_notebook.sh
+++ open_ipython_notebook.sh
@@ -1,2 +1,3 @@
 export pythonexe=$HOME/anaconda3/bin/python
+export PYTHONPATH=$PYTHONPATH:../../src:../../../pymyinstall/src
 ipython notebook --notebook-dir=_doc/notebooks
--- src/pyquickhelper/helpgen/utils_sphinx_config.py
+++ src/pyquickhelper/helpgen/utils_sphinx_config.py
@@ -4,7 +4,7 @@
 
 """
 
-import sys, os
+import sys, os, site
 
 def ie_layout_html():
     """
@@ -17,14 +17,25 @@ def ie_layout_html():
     """
     tofind = '<meta http-equiv="X-UA-Compatible" content="IE=edge" />'
 
+    sitep = [ _ for _ in site.getsitepackages() if "packages" in _ ]
+    if len(sitep) == 1 :
+        sitep = sitep[0]
+    else:
+        raise FileNotFoundError("unable to find site-packages\n{1}".format("\n".join(site.getsitepackages())) )
+        
+    if not os.path.exists(sitep):
+        raise FileNotFoundError("unable to find site-packages, tried: {0}\nALL:\n{1}".format(sitep, 
+                        "\n".join(site.getsitepackages())) )
+                        
     path = os.path.dirname(sys.executable)
-    layout = os.path.join( path, "Lib","site-packages","sphinx","themes","basic","layout.html")
+    layout = os.path.join( sitep,"sphinx","themes","basic","layout.html")
     if os.path.exists(layout):
         with open(layout, "r", encoding="utf-8") as f :
             content = f.read()
         if tofind not in content:
             alls = [ "unable to find: " + tofind  + " in ",
-                     '  File "{0}", line 1'.format(layout) ]
+                     '  File "{0}", line 1'.format(layout) ,
+                     'see http://www.xavierdupre.fr/blog/2014-10-30_nojs.html']
             raise Exception("\n".join(alls))
         return True
     else:
