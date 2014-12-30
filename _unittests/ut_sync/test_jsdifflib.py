af6219f439ad284f0944a9acac81fcaa81ed3faa
dupre <xavier.dupre@ensae.fr>
2014-12-30 21:16:48 +0100

    fix another unit tests failing due to pymyinstall

--- _unittests/ut_sync/test_jsdifflib.py
+++ _unittests/ut_sync/test_jsdifflib.py
@@ -12,14 +12,7 @@ except ImportError :
     path = os.path.normpath(os.path.abspath( os.path.join( os.path.split(__file__)[0], "..", "..")))
     if path not in sys.path : sys.path.append (path)
     import src
-
-try :
-    import pymyinstall
-except ImportError :
-    path = os.path.normpath(os.path.abspath( os.path.join( os.path.split(__file__)[0], "..", "..", "..", "pymyinstall", "src")))
-    if path not in sys.path : sys.path.append (path)
-    import pymyinstall
-
+    
 from src.pyquickhelper.loghelper.flog        import fLOG
 from src.pyquickhelper.sync.visual_sync      import create_visual_diff_through_html
 
@@ -27,6 +20,19 @@ class TestJsDiffLib(unittest.TestCase):
 
     def test_jsdifflib(self) :
         fLOG (__file__, self._testMethodName, OutputPrint = __name__ == "__main__")
+        
+        try :
+            import pymyinstall
+        except ImportError :
+            path = os.path.normpath(os.path.abspath( os.path.join( os.path.split(__file__)[0], "..", "..", "..", "pymyinstall", "src")))
+            if path not in sys.path : sys.path.append (path)
+            try:
+                import pymyinstall
+            except ImportError :
+                # we skip
+                warnings.warn("unable to test TestJsDiffLib.test_jsdifflib")
+                return
+        
         tt = os.path.split(src.pyquickhelper.sync.visual_sync.__file__)[0]
         ma = os.path.join(tt, "temp_difflibjs", "jsdifflib-master")
         if os.path.exists(ma):
