6f69bb59c7bbe14ac1a9adce7fef906f4b01fdbd
dupre <xavier.dupre@ensae.fr>
2015-01-03 13:16:47 +0100

    remove unnecessary import

--- _unittests/run_unittests.py
+++ _unittests/run_unittests.py
@@ -12,7 +12,7 @@ def main():
         sys.path.append ( os.path.normpath (os.path.abspath(os.path.join( os.path.split(__file__)[0],"..","src"))))
         import pyquickhelper
 
-    from pyquickhelper import fLOG, run_cmd, main_wrapper_tests
+    from pyquickhelper import fLOG, main_wrapper_tests
     fLOG(OutputPrint = True)
     main_wrapper_tests(__file__)
 
