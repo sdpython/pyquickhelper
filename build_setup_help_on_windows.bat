13c211ab9f76dd550712bb51528bd2e94db80e53
dupre <xavier.dupre@ensae.fr>
2015-01-16 11:26:24 +0100

    improve the building script

--- build_setup_help_on_windows.bat
+++ build_setup_help_on_windows.bat
@@ -4,21 +4,41 @@ del /Q dist\*.*
 
 rem unittests with python 3.4
 
-IF EXIST c:\Python34vir GOTO next:
+IF NOT EXIST c:\Python34 GOTO checkinstall64:
+
+:checkinstall:
+IF EXIST c:\Python34vir GOTO nexta:
 mkdir c:\Python34vir
 
-:next:
-IF EXIST c:\Python34vir\install GOTO fullsetup:
+:nexta:
+IF EXIST c:\Python34vir\install GOTO fullsetupa:
 c:\Python34\Scripts\virtualenv c:\Python34vir\install --system-site-packages
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-:fullsetup:
-
+:fullsetupa:
 echo #######################################################
 c:\Python34vir\install\Scripts\python -u setup.py install
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
+
+:checkinstall64:
+IF EXIST c:\Python34_64vir GOTO nextb:
+mkdir c:\Python34_64vir
+
+:nextb:
+IF EXIST c:\Python34_64vir\install GOTO fullsetupb:
+c:\Python34\Scripts\virtualenv c:\Python34_64vir\install --system-site-packages
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+:fullsetupb:
+echo #######################################################
+c:\Python34_64vir\install\Scripts\python -u setup.py install
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo #######################################################
+
+:utpy34:
+IF NOT EXIST c:\Python34 GOTO utpy33_64:
 set pythonexe="c:\Python34\python"
 %pythonexe% -u setup.py clean_space
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -26,14 +46,16 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-rem python 3.3
-
+:utpy33_64:
+IF NOT EXIST c:\Python33_x64 GOTO utpy33:
 set pythonexe="c:\Python33_x64\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
+:utpy33:
+IF NOT EXIST c:\Python33 GOTO utpy34_64:
 set pythonexe="c:\Python33\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py sdist --formats=gztar,zip --verbose
@@ -42,8 +64,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-rem python 3.4
-
+:utpy34_64:
 set pythonexe="c:\Python34_x64\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
@@ -52,7 +73,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-set pythonexe="c:\Python34\python"
+:setup_msi:
+set pythonexe="c:\Python34_x64\python"
 %pythonexe% clean_pyd.py
 %pythonexe% setup.py sdist --formats=gztar,zip --verbose
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -62,12 +84,12 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
-rem help
-
+:documentation:
 %pythonexe% -u setup.py build_sphinx
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
+:copyfiles:
 if not exist dist\html mkdir dist\html
 xcopy /E /C /I /Y _doc\sphinxdoc\build\html dist\html
 if %errorlevel% neq 0 exit /b %errorlevel%
