1fb2168069b3aa0abd87a93b12280dc721833b62
dupre <xavier.dupre@ensae.fr>
2015-01-19 15:51:55 +0100

    fix batch setup file

--- build_setup_help_on_windows.bat
+++ build_setup_help_on_windows.bat
@@ -43,7 +43,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
 :setup33:
-IF NOT EXIST c:\Python33 GOTO setup34_64:
+IF NOT EXIST c:\Python33 GOTO setup33_64:
 set pythonexe="c:\Python33\python"
 %pythonexe% setup.py clean_pyd
 %pythonexe% setup.py bdist_wininst
@@ -51,7 +51,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
 :setup33_64:
-IF NOT EXIST c:\Python33_x64 GOTO setup33:
+IF NOT EXIST c:\Python33_x64 GOTO setup34:
 set pythonexe="c:\Python33_x64\python"
 %pythonexe% setup.py clean_pyd
 %pythonexe% setup.py sdist --formats=gztar,zip --verbose
@@ -61,6 +61,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 echo #######################################################
 
 :setup34:
+IF NOT EXIST c:\Python34 GOTO setup34_x64_msi_wheel:
 set pythonexe="c:\Python34\python"
 %pythonexe% setup.py clean_pyd
 %pythonexe% setup.py build bdist_wininst --plat-name=win-amd64
