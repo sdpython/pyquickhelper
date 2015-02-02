99cb43d0ccdcc21f45627b0a8e6fca995a04d245
dupre <xavier.dupre@ensae.fr>
2015-02-02 23:53:45 +0100

    change version number to 1.0

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ else :
     subversion = 1
 
 project_var_name    = "pyquickhelper"
-sversion            = "0.9"
+sversion            = "1.0"
 versionPython       = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
 path                = "Lib/site-packages/" + project_var_name
 readme              = 'README.rst'
--- src/pyquickhelper/__init__.py
+++ src/pyquickhelper/__init__.py
@@ -7,7 +7,7 @@ import sys
 if sys.version_info[0] < 3 :
     raise ImportError("pyquickhelper only works with Python 3")
 
-__version__ = "0.9"
+__version__ = "1.0"
 __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/pyquickhelper"
 __url__ = "http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/index.html"
