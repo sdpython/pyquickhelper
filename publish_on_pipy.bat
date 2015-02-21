4633e38af9e1b5a54309fd4b1fdf2c7317783df3
dupre <xavier.dupre@ensae.fr>
2015-02-21 13:36:15 +0100

    update publish_on_pypi.bat

--- publish_on_pipy.bat
+++ publish_on_pipy.bat
@@ -1,3 +1,9 @@
+if "%1"=="" goto default_value:
+set pythonexe=%1\python
+goto custom_python:
 
-set pythonexe="c:\python34\python"
+:default_value:
+set pythonexe=c:\Python34\python
+
+:custom_python:
 %pythonexe% setup.py sdist register upload
