7e1454865b625387c3b5b81c17a8ac0b2e44e503
dupre <xavier.dupre@ensae.fr>
2015-04-21 23:37:09 +0200

    update setup.py again

--- publish_on_pipy.bat
+++ publish_on_pipy.bat
@@ -6,7 +6,7 @@ goto custom_python:
 set pythonexe=c:\Python34_x64\python
 
 :custom_python:
-%pythonexe% setup.py rotate --match=.zip -k 0
-%pythonexe% setup.py rotate --match=.tar.gz -k 5
+%pythonexe% setup.py rotate --match=.zip --keep=1
+%pythonexe% setup.py rotate --match=.tar.gz --keep=3
 rem %pythonexe% setup.py sdist register
-%pythonexe% setup.py sdist upload --formats=gztar
+%pythonexe% setup.py sdist --formats=gztar upload
