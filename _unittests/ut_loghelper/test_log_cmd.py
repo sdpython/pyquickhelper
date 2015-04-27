9094568422fb0d2ce365b61d6824e0c702b30db6
dupre <xavier.dupre@ensae.fr>
2015-04-27 21:48:14 +0200

    add a unit test, fix na import issue in a notebook

--- _unittests/ut_loghelper/test_log_cmd.py
+++ _unittests/ut_loghelper/test_log_cmd.py
@@ -21,7 +21,8 @@ except ImportError:
         sys.path.append(path)
     import src
 
-from src.pyquickhelper.loghelper.flog import split_cmp_command, fLOG
+from src.pyquickhelper import get_temp_folder
+from src.pyquickhelper.loghelper.flog import split_cmp_command, fLOG, run_cmd, skip_run_cmd
 
 
 class TestLogFunc (unittest.TestCase):
@@ -38,6 +39,32 @@ class TestLogFunc (unittest.TestCase):
         assert split_cmp_command('"ab cd" ef') == ["ab cd", "ef"]
         assert split_cmp_command('"ab" cd ef') == ["ab", "cd", "ef"]
         assert split_cmp_command('"ab cd ef"') == ["ab cd ef"]
+
+    def test_run_cmd_simple(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        temp = get_temp_folder(__file__, "temp_run_cmd_secure")
+
+        cmd = "dir ."
+        out1, err = run_cmd(cmd, wait=True, fLOG=fLOG)
+        out2, err = run_cmd(cmd, wait=True, communicate=False, fLOG=fLOG)
+
+        fLOG("***", out1)
+        fLOG("***", out2)
+
+        secure = os.path.join(temp, "out_log_secure.txt")
+
+        out3, err = run_cmd(
+            cmd, wait=True, communicate=True, secure=secure, fLOG=fLOG)
+        fLOG("***", out3)
+
+        out, err = skip_run_cmd(cmd, wait=True)
+        assert len(out) == 0
+        assert len(err) == 0
+
 
 if __name__ == "__main__":
     unittest.main()
