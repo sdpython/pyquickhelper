4fc50425f7a66bf5ad539beec9a485fc869349a2
dupre <xavier.dupre@ensae.fr>
2015-05-14 19:54:19 +0200

    fix an issue tkinter on virtual environment

--- src/pyquickhelper/pycode/__init__.py
+++ src/pyquickhelper/pycode/__init__.py
@@ -6,3 +6,4 @@ from .code_helper import remove_extra_spaces_and_pep8, remove_extra_spaces_folde
 from .py3to2 import py3to2_convert_tree, py3to2_convert
 from .utils_tests import get_temp_folder, main_wrapper_tests
 from .setup_helper import process_standard_options_for_setup
+from .tkinter_helper import fix_tkinter_issues_virtualenv
---
+++ src/pyquickhelper/pycode/tkinter_helper.py
@@ -0,0 +1,45 @@
+"""
+@file
+@brief  Helpers for tkinter
+
+.. versionadded:: 1.1
+"""
+import os
+
+
+def fix_tkinter_issues_virtualenv():
+    """
+    fix an issue which happens in a virtual environment,
+    see `Fix Tcl inside a virtualenv on Windows <https://github.com/pypa/virtualenv/pull/627>`_
+    """
+    def location():
+        import numpy
+        site = os.path.dirname(os.path.join(os.path.abspath(numpy.__file__)))
+        site = os.path.join(site, "..", "..", "..", "tcl")
+        return os.path.normpath(site)
+
+    def look_for(where, prefix):
+        lst = sorted(os.listdir(where), reverse=True)
+        l = len(prefix)
+        for _ in lst:
+            if _.startswith(prefix) and "0" <= _[l] <= "9" and ".lib" not in _:
+                return os.path.join(where, _)
+        raise FileNotFoundError("unable to find any folder starting with {0} in {1}\nLIST:\n{2}".format(
+            prefix, where, ", ".join(lst)))
+
+    if "TCL_LIBRARY" not in os.environ:
+        loc = location()
+        p = look_for(loc, "tcl")
+        os.environ["TCL_LIBRARY"] = p
+
+    if "TK_LIBRARY" not in os.environ:
+        loc = location()
+        p = look_for(loc, "tk")
+        os.environ["TK_LIBRARY"] = p
+
+    if "TIX_LIBRARY" not in os.environ:
+        loc = location()
+        p = look_for(loc, "tix")
+        os.environ["TIX_LIBRARY"] = p
+
+    return os.environ["TCL_LIBRARY"], os.environ["TK_LIBRARY"], os.environ["TIX_LIBRARY"]
