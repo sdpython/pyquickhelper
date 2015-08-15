19480d6d603b8f43b30b81d54cf63d58e7b80629
dupre <xavier.dupre@ensae.fr>
2015-08-15 20:23:45 +0200

    fix a unit test for python 2.7

--- _unittests/ut_module/test_readme.py
+++ _unittests/ut_module/test_readme.py
@@ -27,6 +27,9 @@ from src.pyquickhelper import fLOG, get_temp_folder
 from src.pyquickhelper.pycode.venv_helper import create_virtual_env, run_venv_script
 from src.pyquickhelper.helpgen.markdown_helper import yield_sphinx_only_markup_for_pipy
 
+if sys.version_info[0] == 2:
+    from codecs import open
+
 
 class TestReadme(unittest.TestCase):
 
