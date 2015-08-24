f1f0de1ac3e7ee38a45fd3a2836e740dcbe5bd42
dupre <xavier.dupre@ensae.fr>
2015-08-24 23:27:33 +0200

    fix a bug for python 2.7

--- src/pyquickhelper/helpgen/sphinx_main_verification.py
+++ src/pyquickhelper/helpgen/sphinx_main_verification.py
@@ -10,6 +10,9 @@ import warnings
 from ..loghelper.flog import noLOG
 from ..filehelper.synchelper import explore_folder_iterfile
 
+if sys.version_info[0] == 2:
+    from codecs import open
+
 
 class SphinxVerificationException(Exception):
 
