5ffa1596fcc05bcd47b045bed3f9d1f234b1d9fd
dupre <xavier.dupre@ensae.fr>
2015-08-26 21:32:53 +0200

    move a function

--- src/pyquickhelper/pycode/setup_helper.py
+++ src/pyquickhelper/pycode/setup_helper.py
@@ -23,236 +23,6 @@ if sys.version_info[0] == 2:
     from codecs import open
 
 
-def get_script_extension():
-    """
-    returns the scripts extension based on the system it is running on
-
-    @return     bat or sh
-    """
-    if sys.platform.startswith("win"):
-        return "bat"
-    else:
-        return "sh"
-
-
-def get_folder(file_or_folder):
-    """
-    returns the folder which contains ``setup.py``
-
-    @param      file_or_folder      file ``setup.py`` or folder which contains it
-    @return                         folder
-    """
-    file_or_folder = os.path.abspath(file_or_folder)
-    if os.path.isdir(file_or_folder):
-        folder = file_or_folder
-    else:
-        folder = os.path.dirname(file_or_folder)
-    return folder
-
-
-def write_version_for_setup(file_or_folder):
-    """
-    extract the version number,
-    the function writes the files ``version.txt`` in this folder
-
-    @param      file_or_folder      file ``setup.py`` or folder which contains it
-    @return                         version number
-
-    .. versionadded:: 1.1
-    """
-    src = SourceRepository(commandline=True)
-    ffolder = get_folder(file_or_folder)
-    version = src.version(ffolder)
-    if version in ["0", 0, None]:
-        raise Exception("issue with version {0}".format(version))
-
-    # write version number
-    if version is not None:
-        with open(os.path.join(ffolder, "version.txt"), "w") as f:
-            f.write(str(version) + "\n")
-
-    return version
-
-
-def clean_space_for_setup(file_or_folder):
-    """
-    .. index:: pep8
-
-    does some cleaning within the module, apply pep8 rules
-
-    @param      file_or_folder      file ``setup.py`` or folder which contains it
-    @return                         deleted files
-    """
-    ffolder = get_folder(file_or_folder)
-    rem = remove_extra_spaces_folder(
-        ffolder,
-        extensions=[
-            ".py",
-            "rst",
-            ".bat",
-            ".sh"])
-    return rem
-
-
-def standard_help_for_setup(file_or_folder, project_var_name, module_name=None, extra_ext=None,
-                            add_htmlhelp=False):
-    """
-    standard function to generate help assuming they follow the same design
-    as *pyquickhelper*
-
-    @param      file_or_folder      file ``setup.py`` or folder which contains it
-    @param      project_var_name    display name of the module
-    @param      module_name         module name, None if equal to *project_var_name* (``import <module_name>``)
-    @param      extra_ext           extra file extension to process (ex ``["doc"]``)
-    @param      add_htmlhelp        run HTML Help too (only on Windows)
-
-    The function outputs some information through function @see fn fLOG.
-
-    A page will be added for each extra file extension mentioned in *extra_ext* if
-    some of these were found.
-    """
-    if "--help" in sys.argv:
-        print(get_help_usage())
-    else:
-        if module_name is None:
-            module_name = project_var_name
-
-        ffolder = get_folder(file_or_folder)
-        source = os.path.join(ffolder, "_doc", "sphinxdoc", "source")
-
-        if not os.path.exists(source):
-            raise FileNotFoundError(
-                "you must get the source from GitHub to build the documentation,\nfolder {0} "
-                "should exist\n(file_or_folder={1})\n(ffolder={2})\n(cwd={3})".format(source, file_or_folder, ffolder, os.getcwd()))
-
-        fLOG(OutputPrint=True)
-        project_name = os.path.split(
-            os.path.split(os.path.abspath(ffolder))[0])[-1]
-
-        if sys.platform.startswith("win"):
-            generate_help_sphinx(project_name, module_name=module_name,
-                                 layout=["html", "pdf"],
-                                 extra_ext=extra_ext,
-                                 add_htmlhelp=add_htmlhelp)
-        else:
-            # unable to test latex conversion due to adjustbox.sty missing
-            # package
-            generate_help_sphinx(project_name, nbformats=["ipynb", "html", "python", "rst"],
-                                 module_name=project_var_name,
-                                 extra_ext=extra_ext,
-                                 add_htmlhelp=add_htmlhelp)
-
-
-def run_unittests_for_setup(file_or_folder, skip_function=default_skip_function, setup_params=None,
-                            only_setup_hook=False,
-                            coverage_options=None,
-                            coverage_exclude_lines=None):
-    """
-    run the unit tests and compute the coverage, stores
-    the results in ``_doc/sphinxdoc/source/coverage``
-    assuming the module follows the same design as *pyquickhelper*
-
-    @param      file_or_folder          file ``setup.py`` or folder which contains it
-    @param      skip_function           see @see fn main_wrapper_tests
-    @param      setup_params            see @see fn main_wrapper_tests
-    @param      only_setup_hook         see @see fn main_wrapper_tests
-    @param      coverage_options        see @see fn main_wrapper_tests
-    @param      coverage_exclude_lines  see @see fn main_wrapper_tests
-
-    .. versionchanged:: 1.3
-        Parameters *coverage_options*, *coverage_exclude_lines* were added.
-        See function @see fn main_wrapper_tests.
-    """
-    ffolder = get_folder(file_or_folder)
-    funit = os.path.join(ffolder, "_unittests")
-    if not os.path.exists(funit):
-        raise FileNotFoundError(
-            "you must get the source from GitHub to run the unittests,\nfolder {0} should exist".format(funit))
-
-    run_unit = os.path.join(funit, "run_unittests.py")
-    if not os.path.exists(run_unit):
-        raise FileNotFoundError(
-            "the folder {0} should contain run_unittests.py".format(funit))
-
-    fix_tkinter_issues_virtualenv()
-    main_wrapper_tests(
-        run_unit, add_coverage=True, skip_function=skip_function, setup_params=setup_params,
-        only_setup_hook=only_setup_hook, coverage_options=coverage_options,
-        coverage_exclude_lines=coverage_exclude_lines)
-
-
-def copy27_for_setup(file_or_folder):
-    """
-    prepare a copy of the source for Python 2.7,
-    assuming the module follows the same design as *pyquickhelper*
-
-    @param      file_or_folder      file ``setup.py`` or folder which contains it
-    """
-    if sys.version_info[0] < 3:
-        raise Exception("Python needs to be Python3")
-
-    root = get_folder(file_or_folder)
-    root = os.path.normpath(root)
-    dest = os.path.join(root, "dist_module27")
-    py3to2_convert_tree(root, dest)
-
-
-def write_pyproj(file_or_folder, location=None):
-    """
-    create a pyproj project to work with `PTVS <>`_ (Python Tools for Visual Studio)
-
-    @param      file_or_folder      file ``setup.py`` or folder which contains it
-    @param      location            if not None, stores the project into this folder
-
-    This functionality fails with Python 2.7 (encoding).
-    """
-    avoid = ["dist", "build", "dist_module27", "_doc"]
-
-    def filter(name):
-        if os.path.splitext(name)[-1] != ".py":
-            return False
-        if "temp_" in name:
-            return False
-        for a in avoid:
-            if name.startswith(a + "\\"):
-                return False
-            if name.startswith(a + "/"):
-                return False
-        return True
-
-    root = get_folder(file_or_folder)
-    root = os.path.normpath(root)
-    name = os.path.split(root)[-1]
-    if location is None:
-        dest = os.path.join(root, "ptvs_project.pyproj")
-    else:
-        dest = os.path.join(location, "ptvs_project.pyproj")
-    all_files = [os.path.relpath(_, root)
-                 for _ in explore_folder_iterfile(root)]
-    all_files = [_ for _ in all_files if filter(_)]
-    pyproj = get_pyproj_project(name, all_files)
-    with open(dest, "w", encoding="utf8") as f:
-        f.write(pyproj.strip())
-
-
-def process_standard_options_for_setup_help():
-    """
-    print the added options available through this module
-    """
-    print("""
-        Help for options added by pyquickhelper:
-
-        build_script    produce various scripts to build the module
-        clean_space     clean unnecessary spaces in the code
-        write_version   write a file ``version.txt`` with the version number (needs an access to GitHub)
-        clean_pyd       clean file ``*.pyd``
-        build_sphinx    build the documentation
-        unittests       run the unit tests
-        copy27          create a modified copy of the module to run on Python 2.7
-
-        """)
-
-
 def process_standard_options_for_setup(argv,
                                        file_or_folder,
                                        project_var_name,
@@ -501,6 +271,236 @@ def process_standard_options_for_setup(argv,
 
     else:
         return False
+
+
+def get_script_extension():
+    """
+    returns the scripts extension based on the system it is running on
+
+    @return     bat or sh
+    """
+    if sys.platform.startswith("win"):
+        return "bat"
+    else:
+        return "sh"
+
+
+def get_folder(file_or_folder):
+    """
+    returns the folder which contains ``setup.py``
+
+    @param      file_or_folder      file ``setup.py`` or folder which contains it
+    @return                         folder
+    """
+    file_or_folder = os.path.abspath(file_or_folder)
+    if os.path.isdir(file_or_folder):
+        folder = file_or_folder
+    else:
+        folder = os.path.dirname(file_or_folder)
+    return folder
+
+
+def write_version_for_setup(file_or_folder):
+    """
+    extract the version number,
+    the function writes the files ``version.txt`` in this folder
+
+    @param      file_or_folder      file ``setup.py`` or folder which contains it
+    @return                         version number
+
+    .. versionadded:: 1.1
+    """
+    src = SourceRepository(commandline=True)
+    ffolder = get_folder(file_or_folder)
+    version = src.version(ffolder)
+    if version in ["0", 0, None]:
+        raise Exception("issue with version {0}".format(version))
+
+    # write version number
+    if version is not None:
+        with open(os.path.join(ffolder, "version.txt"), "w") as f:
+            f.write(str(version) + "\n")
+
+    return version
+
+
+def clean_space_for_setup(file_or_folder):
+    """
+    .. index:: pep8
+
+    does some cleaning within the module, apply pep8 rules
+
+    @param      file_or_folder      file ``setup.py`` or folder which contains it
+    @return                         deleted files
+    """
+    ffolder = get_folder(file_or_folder)
+    rem = remove_extra_spaces_folder(
+        ffolder,
+        extensions=[
+            ".py",
+            "rst",
+            ".bat",
+            ".sh"])
+    return rem
+
+
+def standard_help_for_setup(file_or_folder, project_var_name, module_name=None, extra_ext=None,
+                            add_htmlhelp=False):
+    """
+    standard function to generate help assuming they follow the same design
+    as *pyquickhelper*
+
+    @param      file_or_folder      file ``setup.py`` or folder which contains it
+    @param      project_var_name    display name of the module
+    @param      module_name         module name, None if equal to *project_var_name* (``import <module_name>``)
+    @param      extra_ext           extra file extension to process (ex ``["doc"]``)
+    @param      add_htmlhelp        run HTML Help too (only on Windows)
+
+    The function outputs some information through function @see fn fLOG.
+
+    A page will be added for each extra file extension mentioned in *extra_ext* if
+    some of these were found.
+    """
+    if "--help" in sys.argv:
+        print(get_help_usage())
+    else:
+        if module_name is None:
+            module_name = project_var_name
+
+        ffolder = get_folder(file_or_folder)
+        source = os.path.join(ffolder, "_doc", "sphinxdoc", "source")
+
+        if not os.path.exists(source):
+            raise FileNotFoundError(
+                "you must get the source from GitHub to build the documentation,\nfolder {0} "
+                "should exist\n(file_or_folder={1})\n(ffolder={2})\n(cwd={3})".format(source, file_or_folder, ffolder, os.getcwd()))
+
+        fLOG(OutputPrint=True)
+        project_name = os.path.split(
+            os.path.split(os.path.abspath(ffolder))[0])[-1]
+
+        if sys.platform.startswith("win"):
+            generate_help_sphinx(project_name, module_name=module_name,
+                                 layout=["html", "pdf"],
+                                 extra_ext=extra_ext,
+                                 add_htmlhelp=add_htmlhelp)
+        else:
+            # unable to test latex conversion due to adjustbox.sty missing
+            # package
+            generate_help_sphinx(project_name, nbformats=["ipynb", "html", "python", "rst"],
+                                 module_name=project_var_name,
+                                 extra_ext=extra_ext,
+                                 add_htmlhelp=add_htmlhelp)
+
+
+def run_unittests_for_setup(file_or_folder, skip_function=default_skip_function, setup_params=None,
+                            only_setup_hook=False,
+                            coverage_options=None,
+                            coverage_exclude_lines=None):
+    """
+    run the unit tests and compute the coverage, stores
+    the results in ``_doc/sphinxdoc/source/coverage``
+    assuming the module follows the same design as *pyquickhelper*
+
+    @param      file_or_folder          file ``setup.py`` or folder which contains it
+    @param      skip_function           see @see fn main_wrapper_tests
+    @param      setup_params            see @see fn main_wrapper_tests
+    @param      only_setup_hook         see @see fn main_wrapper_tests
+    @param      coverage_options        see @see fn main_wrapper_tests
+    @param      coverage_exclude_lines  see @see fn main_wrapper_tests
+
+    .. versionchanged:: 1.3
+        Parameters *coverage_options*, *coverage_exclude_lines* were added.
+        See function @see fn main_wrapper_tests.
+    """
+    ffolder = get_folder(file_or_folder)
+    funit = os.path.join(ffolder, "_unittests")
+    if not os.path.exists(funit):
+        raise FileNotFoundError(
+            "you must get the source from GitHub to run the unittests,\nfolder {0} should exist".format(funit))
+
+    run_unit = os.path.join(funit, "run_unittests.py")
+    if not os.path.exists(run_unit):
+        raise FileNotFoundError(
+            "the folder {0} should contain run_unittests.py".format(funit))
+
+    fix_tkinter_issues_virtualenv()
+    main_wrapper_tests(
+        run_unit, add_coverage=True, skip_function=skip_function, setup_params=setup_params,
+        only_setup_hook=only_setup_hook, coverage_options=coverage_options,
+        coverage_exclude_lines=coverage_exclude_lines)
+
+
+def copy27_for_setup(file_or_folder):
+    """
+    prepare a copy of the source for Python 2.7,
+    assuming the module follows the same design as *pyquickhelper*
+
+    @param      file_or_folder      file ``setup.py`` or folder which contains it
+    """
+    if sys.version_info[0] < 3:
+        raise Exception("Python needs to be Python3")
+
+    root = get_folder(file_or_folder)
+    root = os.path.normpath(root)
+    dest = os.path.join(root, "dist_module27")
+    py3to2_convert_tree(root, dest)
+
+
+def write_pyproj(file_or_folder, location=None):
+    """
+    create a pyproj project to work with `PTVS <>`_ (Python Tools for Visual Studio)
+
+    @param      file_or_folder      file ``setup.py`` or folder which contains it
+    @param      location            if not None, stores the project into this folder
+
+    This functionality fails with Python 2.7 (encoding).
+    """
+    avoid = ["dist", "build", "dist_module27", "_doc"]
+
+    def filter(name):
+        if os.path.splitext(name)[-1] != ".py":
+            return False
+        if "temp_" in name:
+            return False
+        for a in avoid:
+            if name.startswith(a + "\\"):
+                return False
+            if name.startswith(a + "/"):
+                return False
+        return True
+
+    root = get_folder(file_or_folder)
+    root = os.path.normpath(root)
+    name = os.path.split(root)[-1]
+    if location is None:
+        dest = os.path.join(root, "ptvs_project.pyproj")
+    else:
+        dest = os.path.join(location, "ptvs_project.pyproj")
+    all_files = [os.path.relpath(_, root)
+                 for _ in explore_folder_iterfile(root)]
+    all_files = [_ for _ in all_files if filter(_)]
+    pyproj = get_pyproj_project(name, all_files)
+    with open(dest, "w", encoding="utf8") as f:
+        f.write(pyproj.strip())
+
+
+def process_standard_options_for_setup_help():
+    """
+    print the added options available through this module
+    """
+    print("""
+        Help for options added by pyquickhelper:
+
+        build_script    produce various scripts to build the module
+        clean_space     clean unnecessary spaces in the code
+        write_version   write a file ``version.txt`` with the version number (needs an access to GitHub)
+        clean_pyd       clean file ``*.pyd``
+        build_sphinx    build the documentation
+        unittests       run the unit tests
+        copy27          create a modified copy of the module to run on Python 2.7
+
+        """)
 
 
 def write_module_scripts(folder, platform=sys.platform, blog_list=None,
