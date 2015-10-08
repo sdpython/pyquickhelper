150dcb4e1d9cc26470b82cef9997ec51a5ea63a8
dupre <xavier.dupre@ensae.fr>
2015-10-08 19:25:43 +0200

    allow multiple roots for the documentatino server

--- src/pyquickhelper/serverdoc/server_helper.py
+++ src/pyquickhelper/serverdoc/server_helper.py
@@ -14,25 +14,41 @@ def get_jenkins_mappings(root, loc="dist"):
     the function looks into folder *root* and list
     folder ``root/.*/dist/html.*``
 
-    @param      root        folder
-    @param      loc         *dist* by default (dist in the folder mentioned above)
+    @param      root        folder or list of folders or list of tuple ``(prefix, folder)``
+    @param      loc         *dist* by default (dist in the folder mentioned above),
+                            it could also be ``_doc/sphinxdoc/build``
     @return                 dictionary { "name":folder }
+
+    .. versionchanged:: 1.3
+        Parameter *root* accepts a list of foldes or tuples.
     """
-    root = os.path.abspath(root)
+    if not isinstance(root, list):
+        roots = [root]
+    else:
+        roots = root
+
     maps = {}
-    sub = os.listdir(root)
-    for s in sub:
-        fold = os.path.join(root, s)
-        if os.path.isdir(fold):
-            dist = os.path.join(fold, loc)
-            if os.path.exists(dist):
-                ht = os.listdir(dist)
-                for h in ht:
-                    if h.startswith("html"):
-                        index = os.path.join(dist, h, "index.html")
-                        if os.path.exists(index):
-                            name = s + "-" + h
-                            maps[name] = os.path.join(dist, h)
+    for root in roots:
+        if isinstance(root, tuple):
+            prefix, root = root
+        else:
+            prefix, root = "", root
+        root = os.path.abspath(root)
+        sub = os.listdir(root)
+        for s in sub:
+            fold = os.path.join(root, s)
+            if os.path.isdir(fold):
+                dist = os.path.join(fold, loc)
+                if os.path.exists(dist):
+                    ht = os.listdir(dist)
+                    for h in ht:
+                        if h.startswith("html"):
+                            index = os.path.join(dist, h, "index.html")
+                            if os.path.exists(index):
+                                name = prefix + s + "-" + \
+                                    h.replace("/", "_").replace("\\", "_")
+                                maps[name] = os.path.join(dist, h)
+
     if len(maps) == 0:
         warnings.warn("unable to find any folder in: " + root)
     return maps
