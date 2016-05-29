ff99b024c9c56d00de8af98f912606c585332810
dupre <xavier.dupre@ensae.fr>
2016-05-29 21:51:01 +0200

    update logging

--- src/pyquickhelper/filehelper/compression_helper.py
+++ src/pyquickhelper/filehelper/compression_helper.py
@@ -108,7 +108,7 @@ def unzip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
                             tos = tos.replace("/", "\\")
                         finalfolder = os.path.split(tos)[0]
                         if not os.path.exists(finalfolder):
-                            fLOG("    creating folder ", finalfolder)
+                            fLOG("    creating folder (zip)", os.path.abspath(finalfolder))
                             try:
                                 os.makedirs(finalfolder)
                             except FileNotFoundError as e:
@@ -133,7 +133,7 @@ def unzip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
                                     tos = os.path.join(where_to, newname)
                                     finalfolder = os.path.split(tos)[0]
                                     if not os.path.exists(finalfolder):
-                                        fLOG("    creating folder ", finalfolder)
+                                        fLOG("    creating folder (zip)", os.path.abspath(finalfolder))
                                         os.makedirs(finalfolder)
                                     with open(tos, "wb") as u:
                                         u.write(data)
@@ -289,7 +289,7 @@ def un7zip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True
                         tos = tos.replace("/", "\\")
                     finalfolder = os.path.split(tos)[0]
                     if not os.path.exists(finalfolder):
-                        fLOG("    creating folder ", finalfolder)
+                        fLOG("    creating folder (7z)", os.path.abspath(finalfolder))
                         try:
                             os.makedirs(finalfolder)
                         except FileNotFoundError as e:
@@ -314,7 +314,7 @@ def un7zip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True
                                 tos = os.path.join(where_to, newname)
                                 finalfolder = os.path.split(tos)[0]
                                 if not os.path.exists(finalfolder):
-                                    fLOG("    creating folder ", finalfolder)
+                                    fLOG("    creating folder (7z)", os.path.abspath(finalfolder))
                                     os.makedirs(finalfolder)
                                 with open(tos, "wb") as u:
                                     u.write(data)
