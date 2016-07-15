3322b31e4aecc1b6c88e3f7b88d0e8f9e62f753d
dupre <xavier.dupre@ensae.fr>
2016-07-15 19:10:59 +0200

    pep8

--- _unittests/ut_helpgen/test_full_documentation_module_template.py
+++ _unittests/ut_helpgen/test_full_documentation_module_template.py
@@ -93,11 +93,11 @@ class TestSphinxDocFull (unittest.TestCase):
                                  use_run_cmd=True)
 
             files = [os.path.join(root, "_doc", "sphinxdoc", "build", "html", "index.html"),
-                os.path.join(
-                root, "_doc", "sphinxdoc", "build", "html", "all_indexes.html"),
-                os.path.join(
-                root, "_doc", "sphinxdoc", "build", "html", "all_notebooks.html"),
-            ]
+                     os.path.join(root, "_doc", "sphinxdoc",
+                                  "build", "html", "all_indexes.html"),
+                     os.path.join(root, "_doc", "sphinxdoc",
+                                  "build", "html", "all_notebooks.html"),
+                     ]
             for f in files:
                 if not os.path.exists(f):
                     raise FileNotFoundError(f)
