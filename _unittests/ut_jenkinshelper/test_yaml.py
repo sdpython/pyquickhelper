302a2ad28383c7faaa3291824ff02dddbed38976
dupre <xavier.dupre@ensae.fr>
2016-08-18 01:07:38 +0200

    update for local yaml

--- _unittests/ut_jenkinshelper/test_yaml.py
+++ _unittests/ut_jenkinshelper/test_yaml.py
@@ -22,7 +22,7 @@ except ImportError:
     import src
 
 from src.pyquickhelper.loghelper import fLOG
-from src.pyquickhelper.jenkinshelper.yaml_helper import load_yaml
+from src.pyquickhelper.jenkinshelper.yaml_helper import load_yaml, enumerate_convert_yaml_into_instructions, evaluate_condition, convert_sequence_into_batch_file
 
 if sys.version_info[0] == 2:
     FileNotFoundError = Exception
@@ -43,12 +43,90 @@ class TestYaml(unittest.TestCase):
             raise FileNotFoundError(yml)
         context = dict(Python34=None, Python35=sys.executable,
                        Python27=None, Anaconda3=None, Anaconda2=None,
-                       WinPython35=None, project_name="pyquickhelper")
+                       WinPython35=None, project_name="pyquickhelper",
+                       root_path="ROOT")
         obj = load_yaml(yml, context=context)
         for k, v in obj.items():
             fLOG(k, type(v), v)
         assert "python" in obj
         assert isinstance(obj["python"], list)
+
+    def test_evaluate_condition(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        r = evaluate_condition('[ ${PYTHON} == "C:\\Python35_x64\\pythonw.exe" ]',
+                               dict(python="C:\\Python35_x64\\pythonw.exe"))
+        assert r
+        assert isinstance(r, bool)
+        r = evaluate_condition('${PYTHON} == "C:\\Python35_x64\\pythonw.exe"',
+                               dict(python="C:\\Python35_x64\\pythonw.exe"))
+        assert r
+        assert isinstance(r, bool)
+        r = evaluate_condition('${PYTHON} != "C:\\Python35_x64\\pythonw.exe"',
+                               dict(python="C:\\Python35_x64\\pythonw.exe"))
+        assert not r
+        assert isinstance(r, bool)
+        r = evaluate_condition('[${PYTHON} != "C:\\Python35_x64\\pythonw.exe", ${PYTHON} == "C:\\Python35_x64\\pythonw.exe"]',
+                               dict(python="C:\\Python35_x64\\pythonw.exe"))
+        assert not r
+        assert isinstance(r, bool)
+
+    def test_jenkins_job_multiplication(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        this = os.path.abspath(os.path.dirname(__file__))
+        yml = os.path.abspath(os.path.join(
+            this, "..", "..", ".local_jenkins.win.yml"))
+        if not os.path.exists(yml):
+            raise FileNotFoundError(yml)
+        context = dict(Python34="fake", Python35=sys.executable,
+                       Python27=None, Anaconda3=None, Anaconda2=None,
+                       WinPython35=None, project_name="pyquickhelper",
+                       root_path="ROOT")
+        obj = load_yaml(yml, context=context)
+        res = list(enumerate_convert_yaml_into_instructions(obj))
+        fLOG(len(res))
+        for r in res:
+            if None in r:
+                raise Exception(r)
+            if r[0][0] != "python":
+                raise Exception(r)
+        if len(res) != 6:
+            rows = [str(_) for _ in res]
+            raise Exception("\n".join(rows))
+        doc = [[s[0] for s in seq if s[1] is not None] for seq in res]
+        fLOG("------", doc)
+        doc = [s for s in doc if "documentation" in s]
+        self.assertEqual(len(doc), 3)
+
+    def test_jconvert_sequence_into_batch_file(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        this = os.path.abspath(os.path.dirname(__file__))
+        yml = os.path.abspath(os.path.join(
+            this, "..", "..", ".local_jenkins.win.yml"))
+        if not os.path.exists(yml):
+            raise FileNotFoundError(yml)
+        context = dict(Python34="fake", Python35=sys.executable,
+                       Python27=None, Anaconda3=None, Anaconda2=None,
+                       WinPython35=None, project_name="pyquickhelper",
+                       root_path="ROOT")
+        obj = load_yaml(yml, context=context)
+        res = list(enumerate_convert_yaml_into_instructions(obj))
+        for r in res:
+            conv = convert_sequence_into_batch_file(r)
+            fLOG("####", conv)
+            assert isinstance(conv, str)
+        assert len(res) > 0
 
 
 if __name__ == "__main__":
--- src/pyquickhelper/jenkinshelper/yaml_helper.py
+++ src/pyquickhelper/jenkinshelper/yaml_helper.py
@@ -15,6 +15,8 @@
 .. versionadded:: 1.4
 """
 import os
+import re
+import sys
 import yaml
 from ..texthelper.templating import apply_template
 
@@ -36,3 +38,225 @@ def load_yaml(file_or_buffer, context=None, engine="jinja2"):
         context = dict()
     file_or_buffer = apply_template(file_or_buffer, context, engine)
     return yaml.load(file_or_buffer)
+
+
+def evaluate_condition(cond, variables=None):
+    """
+    evaluate a condition inserted in a yaml file
+
+    @param      cond        (str) condition
+    @param      variables   (dict|None) dictionary
+    @return                 boolean
+
+    Example of a condition::
+
+        [ ${PYTHON} == "C:\\Python35_x64\\pythonw.exe" ]
+    """
+    if variables is not None:
+        for k, v in variables.items():
+            rep = "${%s}" % k
+            vv = '"%s"' % v
+            cond = cond.replace(rep, vv)
+            cond = cond.replace(rep.upper(), vv)
+    cond = cond.strip()
+    if cond.startswith("[") and cond.endswith("]"):
+        e = eval(cond)
+        return all(e)
+    else:
+        return eval(cond)
+
+
+def interpret_instruction(inst, variables=None):
+    """
+    interpret an instruction with if statement
+
+    @param      inst        (str) instruction
+    @param      variables   (dict|None)
+    @return                 (str|None)
+
+    Example of a statement::
+
+        if [ ${PYTHON} == "C:\\\\Python35_x64\\\\pythonw.exe" ] then python setup.py build_sphinx fi
+    """
+    if isinstance(inst, list):
+        res = [interpret_instruction(_, variables) for _ in inst]
+        if len(res) == 1 and res[0] is None:
+            return None
+        else:
+            return res
+    elif isinstance(inst, tuple):
+        return (inst[0], interpret_instruction(inst[1], variables))
+    elif isinstance(inst, dict):
+        return inst
+    else:
+        inst = inst.replace("\n", " ")
+        exp = re.compile("^ *if +(.*) +then +(.*)( +else +(.*))? +fi *$")
+        find = exp.search(inst)
+        if find:
+            gr = find.groups()
+            e = evaluate_condition(gr[0], variables)
+            return gr[1] if e else gr[3]
+        else:
+            return inst
+
+
+def enumerate_convert_yaml_into_instructions(obj):
+    """
+    convert a yaml file into sequences of instructions
+
+    @param      obj     yaml objects (@see fn load_yaml)
+    @return             list of instructions
+
+    The function expects the following list
+    of steps in this order:
+
+    * language: should be python
+    * python: list of interpreters (multiplies jobs)
+    * virtualenv: name of the virtual environment
+    * install: list of installation steps in the virtual environment
+    * before_script: list of steps to run
+    * script: list of script to run (multiplies jobs)
+    * after_script: list of steps to run
+    * documentation: documentation to run after the
+
+    Each step *multiplies jobs* creates a sequence of jobs
+    and a Jenkins job.
+    """
+    sequences = []
+    count = {}
+    for key in ["language", "python", "virtualenv", "install",
+                "before_script", "script", "after_script",
+                "documentation"]:
+        value = obj.get(key, None)
+        if key == "language":
+            if value != "python":
+                raise NotImplementedError("language must be python")
+            continue
+        elif value is not None:
+            if key in {'python', 'script'} and not isinstance(value, list):
+                value = [value]
+            count[key] = len(value)
+            sequences.append((key, value))
+
+    # multiplications
+    i_python = 0
+    i_script = 0
+    notstop = True
+    while notstop:
+        seq = []
+        add = True
+        variables = {}
+        for key, value in sequences:
+            if key == "python":
+                value = value[i_python]
+            elif key == "script":
+                value = value[i_script]
+                i_script += 1
+                if i_script >= count['script']:
+                    i_script = 0
+                    i_python += 1
+                    if i_python >= count['python']:
+                        notstop = False
+            if value is not None and value != 'None' and value != "conda|":
+                seq.append((key, value))
+                variables[key] = value
+            else:
+                add = False
+        if add:
+            yield interpret_instruction(seq, variables)
+
+
+def ospathjoin(*l, platform=None):
+    """
+    simple ``o.path.join`` for a specific platform
+
+    @param      l           list of paths
+    @param      platform    platform
+    @return                 path
+    """
+    if platform is None:
+        return os.path.join(*l)
+    elif platform.startswith("win"):
+        return "\\".join(l)
+    else:
+        return "/".join(l)
+
+
+def convert_sequence_into_batch_file(seq, platform=None):
+    """
+    converts a sequence of instructions into a batch file
+
+    @param      seq         sequence of instructions
+    @param      platform    ``sys.platform`` if None
+    @return                 (str) batch file
+    """
+    if platform is None:
+        platform = sys.platform
+    rows = []
+    if platform.startswith("win"):
+        error_level = "if %errorlevel% neq 0 exit /b %errorlevel%"
+        interpreter = None
+        pip = None
+        venv = None
+        anaconda = False
+        conda = None
+        pinter = None
+        for key, value in seq:
+            if key == "python":
+                if value.startswith("conda|"):
+                    anaconda = True
+                    interpreter = ospathjoin(
+                        value[6:], "python.exe", platform=platform)
+                    pip = ospathjoin(value[6:], "Scripts",
+                                     "pip.exe", platform=platform)
+                    venv = ospathjoin(
+                        value[6:], "Scripts", "virtualenv.exe", platform=platform)
+                    conda = ospathjoin(
+                        value[6:], "Scripts", "conda.exe", platform=platform)
+                else:
+                    interpreter = ospathjoin(
+                        value, "python.exe", platform=platform)
+                    pip = ospathjoin(value, "Scripts",
+                                     "pip.exe", platform=platform)
+                    venv = ospathjoin(value, "Scripts",
+                                      "virtualenv.exe", platform=platform)
+                pinter = value
+            elif key == "virtualenv":
+                if isinstance(value, list):
+                    if len(value) != 1:
+                        raise ValueError(
+                            "Expecting one value for the path of the virtual environment:\n{0}".format(value))
+                    value = value[0]
+                p = value["path"] if isinstance(value, dict) else value
+                rows.append('if not exist "{0}" mkdir "{0}"'.format(p))
+                if anaconda:
+                    rows.append(
+                        '"{0}" create -p "{1}" --clone "{2}" --offline'.format(conda, p, pinter))
+                    interpreter = ospathjoin(
+                        p, "python.exe", platform=platform)
+                    pip = ospathjoin(p, "Scripts", "pip.exe",
+                                     platform=platform)
+                else:
+                    rows.append(
+                        '"{0}" --system-site-packages "{1}"'.format(venv, p))
+                    interpreter = ospathjoin(
+                        p, "Scripts", "python.exe", platform=platform)
+                    pip = ospathjoin(p, "Scripts", "pip.exe",
+                                     platform=platform)
+                rows.append(error_level)
+            elif key == "install":
+                pass
+            elif key == "before_script":
+                pass
+            elif key == "script":
+                pass
+            elif key == "after_script":
+                pass
+            elif key == "documentation":
+                pass
+            else:
+                raise ValueError("unexpected key '{0}'".format(key))
+        return "\n".join(rows)
+    else:
+        raise NotImplementedError(
+            "not implemented (convert_sequence_into_batch_file): " + platform)
