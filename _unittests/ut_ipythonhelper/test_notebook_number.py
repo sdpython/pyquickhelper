5edc13aa4de70a7806c929350e4a20cca654e551
dupre <xavier.dupre@ensae.fr>
2016-08-20 23:54:28 +0200

    fix unit test

--- _unittests/ut_ipythonhelper/test_notebook_number.py
+++ _unittests/ut_ipythonhelper/test_notebook_number.py
@@ -41,7 +41,7 @@ class TestNotebookNumber(unittest.TestCase):
         assert res
         assert os.path.exists(outfile)
         assert '"execution_count": null' in res
-        if is_travis_or_appveyor() == "travis":
+        if is_travis_or_appveyor() != "travis":
             change_file_status(outfile)
             change_file_status(temp, strict=True)
 
