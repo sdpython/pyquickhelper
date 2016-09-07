29ac65fd0b787be8340404abff4f82ba659dceff
dupre <xavier.dupre@ensae.fr>
2016-09-07 21:28:23 +0200

    fix for python 2.7

--- src/pyquickhelper/pycode/venv_helper.py
+++ src/pyquickhelper/pycode/venv_helper.py
@@ -24,7 +24,7 @@ def is_virtual_environment():
 
     .. versionadded:: 1.3
     """
-    return (sys.base_exec_prefix != sys.exec_prefix) or hasattr(sys, 'real_prefix')
+    return (getattr(sys, "base_exec_prefix", sys.exec_prefix) != sys.exec_prefix) or hasattr(sys, 'real_prefix')
 
 
 class NotImplementedErrorFromVirtualEnvironment(NotImplementedError):
@@ -353,8 +353,10 @@ def run_base_script(script, fLOG=noLOG, file=False, is_cmd=False,
 
     if hasattr(sys, 'real_prefix'):
         exe = sys.real_prefix
-    else:
+    elif hasattr(sys, "base_exec_prefix"):
         exe = sys.base_exec_prefix
+    else:
+        exe = sys.exec_prefix
     if sys.platform.startswith("win"):
         exe = os.path.join(exe, "python")
     else:
