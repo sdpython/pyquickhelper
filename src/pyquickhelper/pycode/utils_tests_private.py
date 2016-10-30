a65710c6078d58e848854e681fdb167ab8a62e7a
dupre <xavier.dupre@ensae.fr>
2016-10-30 11:19:38 +0100

    move function default_filter_warning

--- src/pyquickhelper/pycode/utils_tests_private.py
+++ src/pyquickhelper/pycode/utils_tests_private.py
@@ -14,9 +14,10 @@ import re
 import unittest
 import warnings
 from .utils_tests_stringio import StringIOAndFile
-
+from .default_filter_warning import default_filter_warning
 from ..filehelper.synchelper import remove_folder
 from ..loghelper.flog import run_cmd, noLOG
+
 
 if sys.version_info[0] == 2:
     from StringIO import StringIO
@@ -251,92 +252,6 @@ def clean(dir=None, fLOG=noLOG):
         except Exception as e:
             fLOG("unable to remove dir. ", l,
                  " --- ", str(e).replace("\n", " "))
-
-
-def default_filter_warning(w):
-    """
-    filters out warning
-
-    @param      w       warning
-    @return             boolean (True to keep it)
-
-    Interesting fields: ``w.message``, ``w.category``, ``w.filename``, ``w.lineno``.
-
-    .. todoext::
-        :title: filter warnings after the unit tests
-        :tag: done
-        :date: 2016-07-05
-        :hidden:
-        :issue: 19
-        :release: 1.4
-        :cost: 0.2
-
-        Parameter *filter_warning* was added to give users
-        a way to define their own filtering.
-    """
-    if isinstance(w.message, DeprecationWarning):
-        if w.filename.endswith("kernelspec.py"):
-            return False
-        if "jupyter_client" in w.filename:
-            return False
-        if "IPython" in w.filename:
-            if "DisplayFormatter." in str(w.message):
-                return False
-            if "ScriptMagics." in str(w.message):
-                return False
-            if "HistoryManager." in str(w.message):
-                return False
-            if "ProfileDir." in str(w.message):
-                return False
-            if "InteractiveShell." in str(w.message):
-                return False
-            if "on_trait_change" in str(w.message):
-                return False
-            if "PlainTextFormatter." in str(w.message):
-                return False
-            if "Metadata should be set using the .tag()" in str(w.message):
-                return False
-        elif "nbconvert" in w.filename:
-            if "SlidesExporter." in str(w.message):
-                return False
-            if "TemplateExporter." in str(w.message):
-                return False
-            if "HTMLExporter." in str(w.message):
-                return False
-            if "SVG2PDFPreprocessor." in str(w.message):
-                return False
-            if "on_trait_change" in str(w.message):
-                return False
-            if "PresentExporter." in str(w.message):
-                return False
-            if "NbConvertApp." in str(w.message):
-                return False
-            if "RSTExporter." in str(w.message):
-                return False
-            if "PythonExporter." in str(w.message):
-                return False
-            if "LatexExporter." in str(w.message):
-                return False
-            if "Metadata should be set using the .tag()" in str(w.message):
-                return False
-        elif "jupyter_core" in w.filename:
-            if "JupyterApp." in str(w.message):
-                return False
-        elif "docutils" in w.filename:
-            if "'U' mode is deprecated" in str(w.message):
-                return False
-            if "Metadata should be set using the .tag()" in str(w.message):
-                return False
-        elif "sympy" in w.filename:
-            if "inspect.getargspec() is deprecated" in str(w.message):
-                return False
-        elif "_mode_cbc.py" in w.filename:
-            if "will be forbidden in the future" in str(w.message):
-                return False
-    elif isinstance(w.message, ImportWarning):
-        if w.filename.endswith("_bootstrap_external.py"):
-            return False
-    return True
 
 
 def main_run_test(runner, path_test=None, limit_max=1e9, log=False, skip=-1, skip_list=None,
