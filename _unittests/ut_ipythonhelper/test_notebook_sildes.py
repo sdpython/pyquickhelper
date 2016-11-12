c3a4dc5a07ba17cfdfa3db0288a2348dbc418281
xavier dupré <xavier.dupre@gmail.com>
2016-11-12 10:15:57 +0100

    test for travis 5.13

--- _unittests/ut_ipythonhelper/test_notebook_sildes.py
+++ _unittests/ut_ipythonhelper/test_notebook_sildes.py
@@ -29,39 +29,7 @@ if sys.version_info[0] == 2:
     from codecs import open
 
 
-class TestNotebookReadWrite (unittest.TestCase):
-
-    def test_notebook_read_write(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-        if sys.version_info[0] == 2:
-            # written in Python 3
-            return
-        temp = get_temp_folder(__file__, "temp_notebook_read_write")
-        nbfile = os.path.join(temp, "..", "data", "simple_example.ipynb")
-        assert os.path.exists(nbfile)
-        nb = read_nb(nbfile)
-
-        outfile = os.path.join(temp, "out_notebook.ipynb")
-        nb.to_json(outfile)
-        assert os.path.exists(outfile)
-
-        with open(nbfile, "r", encoding="utf8") as f:
-            c1 = f.read().replace("\r", "")
-        with open(outfile, "r", encoding="utf8") as f:
-            c2 = f.read().replace("\r", "")
-        if c1 != c2:
-            l1 = c1.strip("\n ").split("\n")
-            l2 = c2.strip("\n ").split("\n")
-            for i, cc in enumerate(zip(l1, l2)):
-                a, b = cc
-                if a.strip(" \n") != b.strip(" \n"):
-                    raise Exception(
-                        "difference at line {0}\n1: [{1}]-[{3}]\n2: [{2}]-[{4}]".format(i, a, b, type(a), type(b)))
-            if len(l1) != len(l2):
-                raise Exception("different length")
+class TestNotebookSlides(unittest.TestCase):
 
     def test_notebook_iter(self):
         fLOG(
