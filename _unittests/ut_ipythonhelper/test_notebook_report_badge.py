b5ccc277802c40e49d1abeae702ddb1553708ab4
xavier dupré <xavier.dupre@gmail.com>
2017-11-16 22:03:40 +0100

    improve badges, add more unit tests

--- _unittests/ut_ipythonhelper/test_notebook_report_badge.py
+++ _unittests/ut_ipythonhelper/test_notebook_report_badge.py
@@ -37,8 +37,28 @@ class TestNotebookReportBadge(unittest.TestCase):
                             "dump.notebook.pyquickhelper.txt")
         df = notebook_coverage(os.path.dirname(data), data)
         img = os.path.join(temp, "badge-notebook.png")
+        imgs = [img]
         badge_notebook_coverage(df, img)
         self.assertTrue(os.path.exists(img))
+        df['nbcell'] = 1
+        df['nbvalid'] = 1
+        df['nbrun'] = 1
+        img = os.path.join(temp, "badge-notebook2.png")
+        badge_notebook_coverage(df, img)
+        self.assertTrue(os.path.exists(img))
+        imgs.append(img)
+
+        data = os.path.dirname(data)
+        exp = [os.path.join(data, "badge-notebook.png"),
+               os.path.join(data, "badge-notebook2.png")]
+        for img, exp in zip(imgs, exp):
+            with open(img, "rb") as f:
+                c1 = f.read()
+            with open(exp, "rb") as f:
+                c2 = f.read()
+            if c1 != c2:
+                raise Exception(
+                    "Difference in '{0}' and '{1}'".format(img, exp))
 
 
 if __name__ == "__main__":
