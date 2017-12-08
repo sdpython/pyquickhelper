2e25b380eacb7bcffb2ac1eac0c24968caebee14
xavier dupré <xavier.dupre@gmail.com>
2017-12-08 16:40:13 +0100

    better display in unit test

--- _unittests/ut_loghelper/test_process_helper.py
+++ _unittests/ut_loghelper/test_process_helper.py
@@ -35,13 +35,14 @@ class TestProcessHelper(unittest.TestCase):
             OutputPrint=__name__ == "__main__")
 
         if sys.platform.startswith("win"):
-            cmd = "more"
+            cmd = "pause"
         else:
             cmd = 'ls -la | less'
-        temp = get_temp_folder(__file__, "temp_nb_bug_rst")
+        temp = get_temp_folder(__file__, "temp_reap_children")
         clog = CustomLog(temp)
         proc, _ = run_cmd(cmd, wait=False, fLOG=clog)
         self.assertTrue(_ is None)
+        clog('proc={} pid={}'.format(proc, proc.pid))
         ki = reap_children(fLOG=clog, subset={proc.pid})
         clog('ki={0}'.format(ki))
         self.assertTrue(ki is not None)
