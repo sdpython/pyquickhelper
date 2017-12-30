603acb3162d86286e19bbd7b79bd3c40fd7e23eb
xavier dupré <xavier.dupre@gmail.com>
2017-12-30 15:53:01 +0100

    better error message

--- src/pyquickhelper/loghelper/repositories/pygit_helper.py
+++ src/pyquickhelper/loghelper/repositories/pygit_helper.py
@@ -587,12 +587,16 @@ def get_repo_version(path=None, commandline=True, usedate=False, log=False):
             if path is not None:
                 cmd += " \"%s\"" % path
 
-            out, err = run_cmd(cmd, wait=True, encerror="strict",
-                               encoding=sys.stdout.encoding if sys.version_info[
-                                   0] != 2 and sys.stdout is not None else "utf8",
-                               change_path=os.path.split(
-                                   path)[0] if os.path.isfile(path) else path,
-                               log_error=False, shell=sys.platform.startswith("win32"))
+            try:
+                out, err = run_cmd(cmd, wait=True, encerror="strict",
+                                   encoding=sys.stdout.encoding if sys.version_info[
+                                       0] != 2 and sys.stdout is not None else "utf8",
+                                   change_path=os.path.split(
+                                       path)[0] if os.path.isfile(path) else path,
+                                   log_error=False, shell=sys.platform.startswith("win32"))
+            except Exception as e:
+                raise GitException(
+                    "Problem with subprocess. Path is '{0}'\n[OUT]\n{1}\n[ERR]\n{2}".format(path, out, err)) from e
 
             if len(err) > 0:
                 if log:
