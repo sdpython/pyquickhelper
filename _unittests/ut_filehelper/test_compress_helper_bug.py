d5d208bc971a382d9e433f69abad074cf40640bb
xavier dupré <xavier.dupre@gmail.com>
2018-01-09 12:33:21 +0100

    add command line for 7z (LZMA2)

---
+++ _unittests/ut_filehelper/test_compress_helper_bug.py
@@ -0,0 +1,75 @@
+"""
+@brief      test log(time=4s)
+"""
+
+import sys
+import os
+import unittest
+
+if "temp_" in os.path.abspath(__file__):
+    raise ImportError(
+        "this file should not be imported in that location: " +
+        os.path.abspath(__file__))
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+from src.pyquickhelper.loghelper import fLOG
+from src.pyquickhelper.pycode import get_temp_folder, ExtTestCase
+from src.pyquickhelper.filehelper import un7zip_files
+# from src.pyquickhelper.pycode import is_travis_or_appveyor
+
+
+class TestCompressHelperBug(ExtTestCase):
+
+    def test_uncompress_7zip_lzma2(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        import pylzma
+        # use github version, not pypi version (2016-11-11)
+        # this version does not include a fix to read file produced by the
+        # latest version of 7z
+        self.assertTrue(pylzma)
+
+        fold = get_temp_folder(__file__, "temp_uncompress_7zip_lzma2")
+        data = os.path.join(fold, "..", "data", "donnees.7z")
+        self.assertExists(data)
+        files = un7zip_files(data, where_to=fold, fLOG=fLOG)
+        self.assertEqual(len(files), 1)
+        self.assertTrue(files[0].endswith('donnees.txt'))
+
+    def test_uncompress_7zip_lzma2_cmd(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        import pylzma
+        # use github version, not pypi version (2016-11-11)
+        # this version does not include a fix to read file produced by the
+        # latest version of 7z
+        self.assertTrue(pylzma)
+
+        fold = get_temp_folder(__file__, "temp_uncompress_7zip_lzma2_cmd")
+        data = os.path.join(fold, "..", "data", "donnees.7z")
+        self.assertExists(data)
+        files = un7zip_files(data, where_to=fold, fLOG=fLOG, cmd_line=True)
+        self.assertEqual(len(files), 1)
+        self.assertTrue(files[0].endswith('donnees.txt'))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/pyquickhelper/filehelper/compression_helper.py
+++ src/pyquickhelper/filehelper/compression_helper.py
@@ -8,6 +8,7 @@ import zipfile
 import datetime
 import gzip
 import sys
+import warnings
 from io import BytesIO
 
 from ..loghelper.flog import noLOG, run_cmd
@@ -45,7 +46,7 @@ def zip_files(filename, file_set, root=None, fLOG=noLOG):
             if not os.path.exists(file):
                 continue
             if fLOG:
-                fLOG("zip", file)
+                fLOG("[zip_files] '{0}'".format(file))
             st = os.stat(file)
             atime = datetime.datetime.fromtimestamp(st.st_atime)
             mtime = datetime.datetime.fromtimestamp(st.st_mtime)
@@ -54,7 +55,8 @@ def zip_files(filename, file_set, root=None, fLOG=noLOG):
                 while datetime.datetime.fromtimestamp(new_mtime) < a1980:
                     new_mtime += (4 * 3600)  # new modification time
 
-                fLOG("zip_files: changing time timestamp for file ", file)
+                fLOG(
+                    "[zip_files] changing time timestamp for file '{0}'".format(file))
                 os.utime(file, (st.st_atime, new_mtime))
 
             arcname = os.path.relpath(file, root) if root else None
@@ -90,7 +92,7 @@ def unzip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
     with zipfile.ZipFile(zipf, "r") as file:
         for info in file.infolist():
             if fLOG:
-                fLOG("unzip", info.filename)
+                fLOG("[unzip_files] unzip '{0}'".format(info.filename))
             if where_to is None:
                 files.append((info.filename, file.read(info.filename)))
             else:
@@ -104,7 +106,7 @@ def unzip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
                 tos = os.path.join(where_to, clean)
                 if not os.path.exists(tos):
                     if fvalid and not fvalid(info.filename, tos):
-                        fLOG("    skipping", info.filename)
+                        fLOG("[unzip_files]    skipping", info.filename)
                         continue
                     data = file.read(info.filename)
                     # check encoding to avoid characters not allowed in paths
@@ -113,7 +115,7 @@ def unzip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
                             tos = tos.replace("/", "\\")
                         finalfolder = os.path.split(tos)[0]
                         if not os.path.exists(finalfolder):
-                            fLOG("    creating folder (zip)",
+                            fLOG("[unzip_files]    creating folder (zip)",
                                  os.path.abspath(finalfolder))
                             try:
                                 os.makedirs(finalfolder)
@@ -139,13 +141,14 @@ def unzip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
                                     tos = os.path.join(where_to, newname)
                                     finalfolder = os.path.split(tos)[0]
                                     if not os.path.exists(finalfolder):
-                                        fLOG("    creating folder (zip)",
+                                        fLOG("[unzip_files]    creating folder (zip)",
                                              os.path.abspath(finalfolder))
                                         os.makedirs(finalfolder)
                                     with open(tos, "wb") as u:
                                         u.write(data)
                             files.append(tos)
-                            fLOG("    unzipped ", info.filename, " to ", tos)
+                            fLOG("[unzip_files]    unzipped ",
+                                 info.filename, " to ", tos)
                     elif not tos.endswith("/"):
                         files.append(tos)
                 elif not info.filename.endswith("/"):
@@ -289,12 +292,13 @@ def zip7_files(filename_7z, file_set, fLOG=noLOG, temp_folder="."):
         exe, filename_7z, flist)
     out, err = run_cmd(cmd, wait=True)
     if "Error:" in out or not os.path.exists(filename_7z):
-        raise Exception(
+        raise FileException(
             "An error occurred with cmd: '{0}'\nOUT:\n{1}\nERR\n{2}\n----".format(cmd, out, err))
     return len(file_set)
 
 
-def un7zip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True):
+def un7zip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None,
+                 remove_space=True, cmd_line=False):
     """
     Unzips files from a zip archive compress with :epkg:`7z`.
 
@@ -304,92 +308,138 @@ def un7zip_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True
     @param      fvalid          function which takes two paths (zip name, local name) and return True if the file
                                 must be unzipped, False otherwise, if None, the default answer is True
     @param      remove_space    remove spaces in created local path (+ ``',()``)
+    @param      cmd_line        use command line instead of module :epkg:`pylzma`
     @return                     list of unzipped files
 
     The function requires module :epkg:`pylzma`.
 
     .. versionadded:: 1.4
-    """
-    from py7zlib import Archive7z, FormatError
-    if not isinstance(zipf, BytesIO):
-        if sys.version_info[0] == 2:
-            if isinstance(zipf, bytearray):
-                zipf = BytesIO(zipf)
-            else:
-                zipf = open(zipf, "rb")
-        else:
-            if isinstance(zipf, bytes):
-                zipf = BytesIO(zipf)
-            else:
-                zipf = open(zipf, "rb")
 
-    files = []
-    try:
-        file = Archive7z(zipf)
-    except FormatError as e:
-        raise Exception("The module py7zlib on pypi is not up to date (2016/11). You should replace it by "
-                        "using the version on github: https://github.com/fancycode/pylzma/blob/master/py7zlib.py.") from e
-    for info in file.files:
-        if where_to is None:
-            files.append((info.filename, info.read()))
+    .. versionchanged:: 1.6
+        Parameter *cmd_line* was added.
+    """
+    if cmd_line:
+        if not isinstance(zipf, str  # unicode
+                          ):
+            raise TypeError("Cannot use command line unless zipf is a file.")
+        if remove_space:
+            warnings.warn(
+                '[un7zip_files] remove_space and cmd_line are incompatible options.')
+        if fvalid:
+            raise NotImplementedError(
+                'fvalid and cmd_line are incompatible options.')
+        if sys.platform.startswith("win"):
+            exe = r"C:\Program Files\7-Zip\7z.exe"
+            if not os.path.exists(exe):
+                raise FileNotFoundError("unable to find: {0}".format(exe))
+
+            if where_to is None:
+                where_to = os.path.abspath(".")
         else:
-            clean = remove_diacritics(info.filename)
-            if remove_space:
-                clean = clean.replace(" ", "") \
-                             .replace("'", "") \
-                             .replace(",", "_") \
-                             .replace("(", "_") \
-                             .replace(")", "_")
-            tos = os.path.join(where_to, clean)
-            if not os.path.exists(tos):
-                if fvalid and not fvalid(info.filename, tos):
-                    fLOG("    skipping", info.filename)
-                    continue
-                data = info.read()
-                # check encoding to avoid characters not allowed in paths
+            exe = "7z"
+
+        cmd = '"{0}" x "{1}" -o{2}'.format(exe, zipf, where_to)
+        out, err = run_cmd(cmd, wait=True, fLOG=fLOG)
+        if len(err) > 0 or "Error:" in out:
+            raise FileException(
+                "Unable to un-7zip file '{0}'\n--CMD--\n{3}\n--OUT--\n{1}\n--ERR--\n{2}".format(zipf, out, err, cmd))
+
+        return explore_folder(where_to)[1]
+    else:
+        from py7zlib import Archive7z, FormatError
+        file_zipf = None
+        if not isinstance(zipf, BytesIO):
+            file_zipf = zipf
+            if sys.version_info[0] == 2:
+                if isinstance(zipf, bytearray):
+                    zipf = BytesIO(zipf)
+                else:
+                    zipf = open(zipf, "rb")
+            else:
+                if isinstance(zipf, bytes):
+                    zipf = BytesIO(zipf)
+                else:
+                    zipf = open(zipf, "rb")
+
+        files = []
+        try:
+            file = Archive7z(zipf)
+        except FormatError as e:
+            raise FileException(
+                "You should use a modified version available at https://github.com/sdpython/pylzma") from e
+        for info in file.files:
+            if where_to is None:
+                files.append((info.filename, info.read()))
+            else:
+                clean = remove_diacritics(info.filename)
+                if remove_space:
+                    clean = clean.replace(" ", "").replace("'", "") \
+                                 .replace(",", "_").replace("(", "_") \
+                                 .replace(")", "_")
+                tos = os.path.join(where_to, clean)
                 if not os.path.exists(tos):
-                    if sys.platform.startswith("win"):
-                        tos = tos.replace("/", "\\")
-                    finalfolder = os.path.split(tos)[0]
-                    if not os.path.exists(finalfolder):
-                        fLOG("    creating folder (7z)",
-                             os.path.abspath(finalfolder))
-                        try:
-                            os.makedirs(finalfolder)
-                        except FileNotFoundError as e:
-                            mes = "Unexpected error\ninfo.filename={0}\ntos={1}\nfinalfolder={2}\nlen(nfinalfolder)={3}".format(
-                                info.filename, tos, finalfolder, len(finalfolder))
-                            raise FileNotFoundError(mes) from e
-                    if not info.filename.endswith("/"):
-                        try:
-                            with open(tos, "wb") as u:
-                                u.write(data)
-                        except FileNotFoundError as e:
-                            # probably an issue in the path name
-                            # the next lines are just here to distinguish
-                            # between the two cases
-                            if not os.path.exists(finalfolder):
-                                raise e
-                            else:
-                                newname = info.filename.replace(
-                                    " ", "_").replace(",", "_")
-                                if sys.platform.startswith("win"):
-                                    newname = newname.replace("/", "\\")
-                                tos = os.path.join(where_to, newname)
-                                finalfolder = os.path.split(tos)[0]
-                                if not os.path.exists(finalfolder):
-                                    fLOG("    creating folder (7z)",
-                                         os.path.abspath(finalfolder))
-                                    os.makedirs(finalfolder)
+                    if fvalid and not fvalid(info.filename, tos):
+                        fLOG("[un7zip_files]    skipping", info.filename)
+                        continue
+                    try:
+                        data = info.read()
+                    except NotImplementedError as e:
+                        # You should use command line.
+                        if file_zipf is None:
+                            raise TypeError(
+                                "Cannot switch to command line unless zipf is a file.")
+                        warnings.warn(
+                            "[un7zip_files] '{0}' --> Unavailable format. Use command line.".format(zipf))
+                        return un7zip_files(file_zipf, where_to=where_to, fLOG=fLOG, fvalid=fvalid,
+                                            remove_space=remove_space, cmd_line=True)
+                    except Exception as e:
+                        raise FileException("Unable to unzip file '{0}' from '{1}'".format(
+                            info.filename, zipf)) from e
+                    # check encoding to avoid characters not allowed in paths
+                    if not os.path.exists(tos):
+                        if sys.platform.startswith("win"):
+                            tos = tos.replace("/", "\\")
+                        finalfolder = os.path.split(tos)[0]
+                        if not os.path.exists(finalfolder):
+                            fLOG("[un7zip_files]    creating folder (7z)",
+                                 os.path.abspath(finalfolder))
+                            try:
+                                os.makedirs(finalfolder)
+                            except FileNotFoundError as e:
+                                mes = "Unexpected error\ninfo.filename={0}\ntos={1}\nfinalfolder={2}\nlen(nfinalfolder)={3}".format(
+                                    info.filename, tos, finalfolder, len(finalfolder))
+                                raise FileNotFoundError(mes) from e
+                        if not info.filename.endswith("/"):
+                            try:
                                 with open(tos, "wb") as u:
                                     u.write(data)
+                            except FileNotFoundError as e:
+                                # probably an issue in the path name
+                                # the next lines are just here to distinguish
+                                # between the two cases
+                                if not os.path.exists(finalfolder):
+                                    raise e
+                                else:
+                                    newname = info.filename.replace(
+                                        " ", "_").replace(",", "_")
+                                    if sys.platform.startswith("win"):
+                                        newname = newname.replace("/", "\\")
+                                    tos = os.path.join(where_to, newname)
+                                    finalfolder = os.path.split(tos)[0]
+                                    if not os.path.exists(finalfolder):
+                                        fLOG("[un7zip_files]    creating folder (7z)",
+                                             os.path.abspath(finalfolder))
+                                        os.makedirs(finalfolder)
+                                    with open(tos, "wb") as u:
+                                        u.write(data)
+                            files.append(tos)
+                            fLOG("[un7zip_files]    unzipped ",
+                                 info.filename, " to ", tos)
+                    elif not tos.endswith("/"):
                         files.append(tos)
-                        fLOG("    unzipped ", info.filename, " to ", tos)
-                elif not tos.endswith("/"):
+                elif not info.filename.endswith("/"):
                     files.append(tos)
-            elif not info.filename.endswith("/"):
-                files.append(tos)
-    return files
+        return files
 
 
 def unrar_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True):
@@ -430,6 +480,6 @@ def unrar_files(zipf, where_to=None, fLOG=noLOG, fvalid=None, remove_space=True)
         out, err = run_cmd(cmd, wait=True, fLOG=fLOG, change_path=where_to)
         if len(err) > 0:
             raise FileException(
-                "Unable to unrar file '{0}'\nOUT\n{1}\nERR\n{2}".format(zipf, out, err))
+                "Unable to unrar file '{0}'\n--CMD--\n{3}\n--OUT--\n{1}\n--ERR--\n{2}".format(zipf, out, err, cmd))
 
         return explore_folder(where_to)[1]
