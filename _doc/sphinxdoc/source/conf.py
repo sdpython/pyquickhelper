d081c5cfccc81a9b9256c9b5410345e01a285461
xavier dupré <xavier.dupre@gmail.com>
2018-02-03 20:34:09 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -28,5 +28,5 @@ blog_root = "http://www.xavierdupre.fr/app/pyquickhelper/helpsphinx/"
 nbneg_pattern = ".*[\\\\/]temp_.*"
 
 html_context = {
-    'css_files': get_default_stylesheet() + ['_static/my-styles.css'],
+    'css_files': get_default_stylesheet() + ['_static/my-styles.css', '_static/gallery.css'],
 }
