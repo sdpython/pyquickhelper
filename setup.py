7dadd999f5ae2812891626ce342abea48fe7d9f5
xavier dupré <xavier.dupre@gmail.com>
2018-02-16 11:47:28 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages
 #########
 
 project_var_name = "pyquickhelper"
-sversion = "1.6"
+sversion = "1.7"
 versionPython = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
 path = "Lib/site-packages/" + project_var_name
 readme = 'README.rst'
