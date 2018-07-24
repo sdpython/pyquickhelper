dd00da7c1ac56a265d1e336ebb10ab526c727c8e
xavier dupré <xavier.dupre@gmail.com>
2018-07-24 15:26:29 +0200

    disable one unit test for appveyor

--- _unittests/ut_pycode/test_build_script.py
+++ _unittests/ut_pycode/test_build_script.py
@@ -21,22 +21,19 @@ except ImportError:
     import src
 
 from src.pyquickhelper.loghelper import fLOG
-from src.pyquickhelper.pycode import get_temp_folder, is_travis_or_appveyor
+from src.pyquickhelper.pycode import get_temp_folder, skipif_appveyor
 from src.pyquickhelper.pycode.build_helper import get_build_script, get_script_command, get_extra_script_command, _default_nofolder
 from src.pyquickhelper.pycode.setup_helper import write_pyproj
 
 
 class TestBuildScript(unittest.TestCase):
 
+    @skipif_appveyor("__NOFOLDERSHOULDNOTEXIST37__ not replaced")
     def test_build_script(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
-
-        if is_travis_or_appveyor() == "appveyor":
-            # issue
-            return
 
         if sys.platform.startswith("win") and sys.version_info[0] != 2:
             sc = get_build_script("pyquickhelper")
@@ -60,6 +57,7 @@ class TestBuildScript(unittest.TestCase):
             # not yet implemented for this platform
             return
 
+    @skipif_appveyor("__NOFOLDERSHOULDNOTEXIST37__ not replaced")
     def test_build_script_all(self):
         fLOG(
             __file__,
