5a1286e22c0ba1a6ed849058b72e9837579d2301
xavier dupré <xavier.dupre@gmail.com>
2019-01-12 17:47:48 +0100

    Split unit tests

--- _unittests/ut_documentation/test_SKIP_gitjs_run_notebooks.py
+++ _unittests/ut_documentation/test_SKIP_gitjs_run_notebooks.py
@@ -22,7 +22,7 @@ except ImportError:
     import src
 
 from src.pyquickhelper.loghelper import fLOG
-from src.pyquickhelper.pycode import get_temp_folder, skipif_travis
+from src.pyquickhelper.pycode import get_temp_folder
 from src.pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut
 
 
@@ -32,9 +32,7 @@ class TestSKIPRunNotebooks(unittest.TestCase):
         """for pylint"""
         self.assertTrue(src is not None)
 
-    @unittest.skipIf(sys.version_info[0] == 2, reason="notebooks are not converted into python 2.7, so not tested")
-    @skipif_travis("does not complete")
-    def test_skip_run_notebook(self):
+    def test_skip_run_notebook_javascript(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -49,8 +47,38 @@ class TestSKIPRunNotebooks(unittest.TestCase):
             if os.path.splitext(f)[-1] == ".ipynb":
                 if "javascript" in f:
                     keepnote.append(os.path.join(fnb, f))
-                if "git_data" in f and os.environ.get("PYINT", "") == "python3.7":
-                    # the notebook on git takes for ever
+        self.assertTrue(len(keepnote) > 0)
+
+        def valid(cell):
+            return True
+
+        import jyquickhelper
+        addpaths = [os.path.normpath(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "..", "..", "src")),
+            os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(jyquickhelper.__file__)), ".."))]
+
+        try:
+            res = execute_notebook_list(
+                temp, keepnote, fLOG=fLOG, valid=valid, additional_path=addpaths)
+            execute_notebook_list_finalize_ut(
+                res, fLOG=fLOG, dump=src.pyquickhelper)
+        except PermissionError as e:
+            warnings.warn("Unable to test this notebook due to " + str(e))
+
+    def test_skip_run_notebook_git(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        temp = get_temp_folder(__file__, "temp_skip_run_notebooks_pyq_long")
+
+        fnb = os.path.normpath(os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "..", "..", "_doc", "notebooks"))
+        keepnote = []
+        for f in os.listdir(fnb):
+            if os.path.splitext(f)[-1] == ".ipynb":
+                if "git_data" in f:
                     keepnote.append(os.path.join(fnb, f))
         self.assertTrue(len(keepnote) > 0)
 
