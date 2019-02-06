2e82dc2129ff2c40c1957a10cf3ebe6d2cccce01
xavier dupré <xavier.dupre@gmail.com>
2019-02-06 08:52:29 +0100

    pep8

--- src/pyquickhelper/sphinxext/blog_post_list.py
+++ src/pyquickhelper/sphinxext/blog_post_list.py
@@ -309,9 +309,9 @@ class BlogPostList:
             f.write(".. tocdelay::\n")
             f.write("\n")
             for item in self:
-                f.write(
-                    "    {0} - {1} <{2}/{3}>\n".format(item.Date, item.Title, item.Date[:4],
-                                                      os.path.splitext(os.path.split(item.FileName)[-1])[0]))
+                name_file = os.path.splitext(os.path.split(item.FileName)[-1])[0])
+                f.write("    {0} - {1} <{2}/{3}>\n".format(
+                    item.Date, item.Title, item.Date[:4], name_file))
             f.write("\n\n")
             f.write(".. toctree::\n")
             f.write("    :hidden:\n")
