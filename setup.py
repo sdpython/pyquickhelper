9d75950754710e274bdf4082b2864ef6c8814afe
xavier dupré <xavier.dupre@gmail.com>
2019-03-02 13:11:57 +0100

    Fix nbconvert dependency

--- setup.py
+++ setup.py
@@ -246,7 +246,7 @@ if not r:
             "metakernel",
             "multi_key_dict",
             # see https://github.com/sdpython/pyquickhelper/issues/216
-            "git+https://github.com/sdpython/nbconvert.git@fixes",
+            "nbconvert @ git+https://github.com/sdpython/nbconvert.git@fixes",
             "nbformat",
             "nbpresent",
             "notebook>=4.2.0",
