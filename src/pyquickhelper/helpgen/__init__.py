614c8f7bf167591176154d8bc69ab3d6f343fd52
xavier dupré <xavier.dupre@gmail.com>
2019-11-10 18:37:28 +0100

    Removes wrong import for #280

--- src/pyquickhelper/helpgen/__init__.py
+++ src/pyquickhelper/helpgen/__init__.py
@@ -7,7 +7,7 @@ from .default_conf import set_sphinx_variables, custom_setup
 from .helpgen_exceptions import HelpGenException, ImportErrorHelpGen, HelpGenConvertError
 from .help_usage import get_help_usage
 from .pandoc_helper import latex2rst
-from .process_notebook_api import nb2slides, nb2html, nb2present, nb2rst
+from .process_notebook_api import nb2slides, nb2html, nb2rst
 from .rst_converters import rst2html, docstring2html, rst2rst_folder
 from .sphinx_helper import sphinx_add_scripts
 # Disable to speed up import time.
