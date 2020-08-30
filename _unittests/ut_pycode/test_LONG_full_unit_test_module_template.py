deb67ea89b4edf263a4f1034619278cbdfd90a14
xavier dupré <xavier.dupre@gmail.com>
2020-08-30 11:30:17 +0200

    Update test_LONG_full_unit_test_module_template.py

--- _unittests/ut_pycode/test_LONG_full_unit_test_module_template.py
+++ _unittests/ut_pycode/test_LONG_full_unit_test_module_template.py
@@ -11,16 +11,17 @@ from io import StringIO
 import pyquickhelper
 from pyquickhelper.loghelper.flog import fLOG
 from pyquickhelper.loghelper import sys_path_append
-from pyquickhelper.pycode import get_temp_folder, process_standard_options_for_setup, is_travis_or_appveyor
+from pyquickhelper.pycode import (
+    get_temp_folder, process_standard_options_for_setup, is_travis_or_appveyor)
 from pyquickhelper.pycode import ExtTestCase
 from pyquickhelper.pycode.utils_tests import TestWrappedException
+from pyquickhelper.pycode.utils_tests_helper import PEP8Exception
 from pyquickhelper.loghelper import git_clone
 from pyquickhelper import __file__ as pyq_location
 
 
 class TestUnitTestFullModuleTemplate(ExtTestCase):
 
-    @unittest.skipIf(sys.version_info[0] == 2, reason="does not work on Python 2")
     def test_full_unit_test(self):
         fLOG(
             __file__,
@@ -94,102 +95,109 @@ class TestUnitTestFullModuleTemplate(ExtTestCase):
                     # AttributeError: type object 'Callable' has no attribute '_abc_registry'
                     continue
 
-                with self.subTest(command=command):
-                    fLOG("#######################################################")
-                    fLOG("#######################################################")
-                    fLOG(command)
-                    fLOG("#######################################################")
-                    rem = False
-                    PYTHONPATH = os.environ.get("PYTHONPATH", "")
-                    sep = ";" if sys.platform.startswith("win") else ":"
-                    new_val = PYTHONPATH + sep + thispath
-                    new_val_src = new_val + sep + 'src'
-                    if os.path.exists(new_val_src):
-                        new_val = new_val_src
-                    os.environ["PYTHONPATH"] = new_val.strip(sep)
-                    if command == "build_sphinx":
-                        if thispath not in sys.path:
-                            sys.path.append(thispath)
-                            fLOG("UT add", thispath)
-                            rem = True
-                    log_lines = []
+                fLOG("#######################################################")
+                fLOG("#######################################################")
+                fLOG(command)
+                fLOG("#######################################################")
+                rem = False
+                PYTHONPATH = os.environ.get("PYTHONPATH", "")
+                sep = ";" if sys.platform.startswith("win") else ":"
+                new_val = PYTHONPATH + sep + thispath
+                new_val_src = new_val + sep + 'src'
+                if os.path.exists(new_val_src):
+                    new_val = new_val_src
+                os.environ["PYTHONPATH"] = new_val.strip(sep)
+                if command == "build_sphinx":
+                    if thispath not in sys.path:
+                        sys.path.append(thispath)
+                        fLOG("UT add", thispath)
+                        rem = True
+                log_lines = []
 
-                    def logging_custom(*args, **kwargs):
-                        log_lines.append(args)
-                    lcmd = command.split() if ' ' in command else [command]
-                    stdout2 = StringIO()
-                    stderr2 = StringIO()
+                def logging_custom(*args, **kwargs):
+                    log_lines.append(args)
+                lcmd = command.split() if ' ' in command else [command]
+                stdout2 = StringIO()
+                stderr2 = StringIO()
 
-                    pos_remove = None
-                    if command == "unittests -e .*code_style.*":
-                        if pyq_folder not in sys.path:
-                            pos_remove = len(sys.path)
-                            sys.path.append(pyq_folder)
-                            fLOG("ADD='{0}'".format(pyq_folder))
+                pos_remove = None
+                if command == "unittests -e .*code_style.*":
+                    if pyq_folder not in sys.path:
+                        pos_remove = len(sys.path)
+                        sys.path.append(pyq_folder)
+                        fLOG("ADD='{0}'".format(pyq_folder))
 
-                    try:
-                        r = process_standard_options_for_setup(
-                            lcmd, setup, "python3_module_template",
-                            port=8067, requirements=["pyquickhelper"],
-                            blog_list=blog_list,
-                            fLOG=logging_custom,
-                            additional_ut_path=[pyq, (root, True)],
-                            skip_function=skip_function,
-                            coverage_options={"disable_coverage": True},
-                            hook_print=False, stdout=stdout2, stderr=stderr2,
-                            use_run_cmd=True)
-                        goon = True
-                    except TestWrappedException as e:
-                        if "test_coverage_combine.py" in str(e):
-                            goon = False
-                        else:
-                            raise e
-                    except NotImplementedError:
-                        # Maybe not implemented on linux or windows.
+                try:
+                    r = process_standard_options_for_setup(
+                        lcmd, setup, "python3_module_template",
+                        port=8067, requirements=["pyquickhelper"],
+                        blog_list=blog_list,
+                        fLOG=logging_custom,
+                        additional_ut_path=[pyq, (root, True)],
+                        skip_function=skip_function,
+                        coverage_options={"disable_coverage": True},
+                        hook_print=False, stdout=stdout2, stderr=stderr2,
+                        use_run_cmd=True)
+                    goon = True
+                except PEP8Exception as e:
+                    lines = str(e).split('\n')[1:]
+                    lines = [line for line in lines
+                             if "should be placed before" not in line]
+                    content = "\n".join(lines).strip("\n\r\t ")
+                    if len(content) > 0:
+                        raise AssertionError(
+                            "Remaining style issues.\n{}".format(content)) from e
+                except TestWrappedException as e:
+                    if "test_coverage_combine.py" in str(e):
                         goon = False
+                    else:
+                        raise e
+                except NotImplementedError:
+                    # Maybe not implemented on linux or windows.
+                    goon = False
 
-                    if goon:
-                        if command == "unittests -e .*code_style.*" and pos_remove:
-                            if sys.path[pos_remove] != pyq_folder:
-                                raise Exception(
-                                    "sys.path has changed at position {0}".format(pos_remove))
-                            del sys.path[pos_remove]
-                            fLOG("REMOVE='{0}'".format(pyq_folder))
-                    elif pos_remove:
+                if goon:
+                    if command == "unittests -e .*code_style.*" and pos_remove:
+                        if sys.path[pos_remove] != pyq_folder:
+                            raise Exception(
+                                "sys.path has changed at position {0}".format(pos_remove))
                         del sys.path[pos_remove]
+                        fLOG("REMOVE='{0}'".format(pyq_folder))
+                elif pos_remove:
+                    del sys.path[pos_remove]
 
-                    vout = stdout2.getvalue()
-                    stdout.write(vout)
-                    verr = stderr2.getvalue()
-                    stderr.write(verr)
+                vout = stdout2.getvalue()
+                stdout.write(vout)
+                verr = stderr2.getvalue()
+                stderr.write(verr)
 
-                    if "unittests" in command:
-                        if not r:
-                            raise Exception("{0}-{1}".format(r, command))
-                        for line in log_lines:
-                            fLOG("  ", line)
-                        if len(log_lines) == 0:
-                            raise Exception(
-                                "command1={0}\n--OUT--\n{1}\n--ERR--\n{2}".format(command, vout, verr))
-                        if "-e" in command and "running test   1, ut_module/test_convert_notebooks.py" in vout:
-                            raise Exception(vout)
-                        if "-e" in command and "_ext" not in vout and "code_style" not in command:
-                            raise Exception(
-                                "command3={0}\n--OUT--\n{1}".format(command, vout))
-                        if "LONG" in command and "running test   1, ut_module/test_convert_notebooks.py" in vout:
-                            raise Exception(vout)
-                    if rem:
-                        del sys.path[sys.path.index(thispath)]
-                    os.environ["PYTHONPATH"] = PYTHONPATH
+                if "unittests" in command:
+                    if not r:
+                        raise Exception("{0}-{1}".format(r, command))
+                    for line in log_lines:
+                        fLOG("  ", line)
+                    if len(log_lines) == 0:
+                        raise Exception(
+                            "command1={0}\n--OUT--\n{1}\n--ERR--\n{2}".format(command, vout, verr))
+                    if "-e" in command and "running test   1, ut_module/test_convert_notebooks.py" in vout:
+                        raise Exception(vout)
+                    if "-e" in command and "_ext" not in vout and "code_style" not in command:
+                        raise Exception(
+                            "command3={0}\n--OUT--\n{1}".format(command, vout))
+                    if "LONG" in command and "running test   1, ut_module/test_convert_notebooks.py" in vout:
+                        raise Exception(vout)
+                if rem:
+                    del sys.path[sys.path.index(thispath)]
+                os.environ["PYTHONPATH"] = PYTHONPATH
 
-                fLOG("#######################################################")
-                fLOG("#######################################################")
-                fLOG("OUT:\n", stdout.getvalue())
-                fLOG("ERR:\n", stderr.getvalue())
+            fLOG("#######################################################")
+            fLOG("#######################################################")
+            fLOG("OUT:\n", stdout.getvalue())
+            fLOG("ERR:\n", stderr.getvalue())
 
-                out = os.path.join(temp, "_unittests", "unittests.out")
-                if not os.path.exists(out):
-                    raise Exception("not found: " + out)
+            out = os.path.join(temp, "_unittests", "unittests.out")
+            if not os.path.exists(out):
+                raise Exception("not found: " + out)
 
 
 if __name__ == "__main__":
