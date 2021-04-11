f57a539d92000f05c0df0d59bb7590f70aba0f4e
xavier dupré <xavier.dupre@gmail.com>
2021-04-11 17:04:43 +0200

    Improves a couple of files

--- _unittests/ut_helpgen/data_project/pp/_unittests/ut_example/test_example.py
+++ _unittests/ut_helpgen/data_project/pp/_unittests/ut_example/test_example.py
@@ -1,33 +1,16 @@
 """
 @brief      test log(time=1s)
-
-You should indicate a time in seconds. The program ``run_unittests.py``
-will sort all test files by increasing time and run them.
 """
-
 import unittest
 from pyquickhelper.pycode import ExtTestCase
-
-from python3_module_template.subproject.myexample import myclass
-from python3_module_template.subproject.myexampleb import myclassb
-from python3_module_template.subproject2.myexample2 import myclass2
+from python3_module_template import fct
 
 
 class TestExample(ExtTestCase):
     """Example of a test."""
 
-    def test_split_cmp_command(self):
-        my = myclass(4)
-        r = my.get_value(5)
-        ex = 20
-        if r != ex:
-            raise Exception("we expect %f, not %f" % (ex, r))
-        my2 = myclass2(5)
-        my3 = myclassb(4)
-        self.assertTrue(my2 is not None)
-        self.assertTrue(my3 is not None)
-        mul = my2.get_value(3)
-        self.assertEqual(mul, 15)
+    def test_fct(self):
+        fct()
 
 
 if __name__ == "__main__":
---
+++ _unittests/ut_helpgen/test_sphinx_main_documentation.py
@@ -0,0 +1,35 @@
+"""
+@brief      test log(time=287s)
+@author     Xavier Dupre
+"""
+import sys
+import os
+import unittest
+import warnings
+import logging
+from docutils.parsers.rst import roles
+from sphinx.util.logging import getLogger
+import pyquickhelper
+from pyquickhelper.loghelper.flog import fLOG, download
+from pyquickhelper.loghelper import CustomLog, sys_path_append
+from pyquickhelper.helpgen.sphinx_main import generate_help_sphinx
+from pyquickhelper.pycode import get_temp_folder
+
+
+class TestSphinxMainDocumentation(unittest.TestCase):
+
+    def test_sphinx_main_documentation(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        temp = get_temp_folder(__file__, "temp_sphinx_main", clean=__name__ != "__main__")
+        root = os.path.join(temp, "..", "data_project", "pp")
+        var = "python3_module_template"
+        generate_help_sphinx(var, module_name=var, root=root,
+                             parallel=1, fLOG=fLOG)
+
+
+if __name__ == "__main__":
+    unittest.main()
