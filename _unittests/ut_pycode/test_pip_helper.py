d34f96158c1cb4666292389ba4d000807484bbcb
xavier dupré <xavier.dupre@gmail.com>
2021-10-12 10:26:50 +0200

    fix issue with latest pip

--- _unittests/ut_pycode/test_pip_helper.py
+++ _unittests/ut_pycode/test_pip_helper.py
@@ -1,37 +1,23 @@
 """
 @brief      test tree node (time=2s)
 """
-
-import sys
-import os
 import unittest
 import pandas
-
-from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode.pip_helper import get_packages_list, package2dict
 from pyquickhelper.pycode import ExtTestCase
-from pyquickhelper.pycode.pip_helper import PQPipError
+from pyquickhelper.pycode.pip_helper import (
+    get_packages_list, package2dict, get_package_info,
+    PQPipError)
 
 
 class TestPipHelper(ExtTestCase):
 
     def test_exc(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-
         exc = PQPipError('cmd', 'out', 'err')
         msg = str(exc)
         self.assertEqual([msg.replace('\n', '')], [
                          'CMD:cmdOUT:out[piperror]err'])
 
     def test_pip_list(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-
         li = get_packages_list()
         dt = package2dict(li[0])
         avoid = {'py_version'}
@@ -42,6 +28,25 @@ class TestPipHelper(ExtTestCase):
                     empty.append(k)
         self.assertEmpty(empty)
         self.assertNotEmpty(li)
+
+    def test_pip_show(self):
+        info = get_package_info("pandas")
+        if "version" not in str(info):
+            raise AssertionError(str(info))
+
+        info = get_package_info("sphinx")
+        if "version" not in str(info):
+            raise Exception(str(info))
+
+    def test_pip_show_all(self):
+        info = get_package_info(start=0, end=2)
+        df = pandas.DataFrame(info)
+        self.assertNotEmpty(info)
+
+        if __name__ == "__main__":
+            info = get_package_info()
+            df = pandas.DataFrame(info)
+            df.to_excel("out_packages.xlsx")
 
 
 if __name__ == "__main__":
--- src/pyquickhelper/pycode/pip_helper.py
+++ src/pyquickhelper/pycode/pip_helper.py
@@ -26,11 +26,82 @@ class PQPipError(Exception):
             Exception.__init__(self, mes)
 
 
+class Distribution:
+    """
+    Common interface for old and recent pip packages.
+
+    .. versionadded:: 1.5
+    """
+
+    def __init__(self, dist):
+        self.dist = dist
+
+    def __getattr__(self, attr):
+        if attr == 'key':
+            if hasattr(self.__dict__['dist'], 'key'):
+                return self.__dict__['dist'].key
+            return self.__dict__['dist'].canonical_name
+        if attr == 'dist':
+            return self.__dict__['dist']
+        if attr in {'_get_metadata', 'requires', 'PKG_INFO', 'project_name',
+                    'py_version', 'platform', 'extras'}:
+            if hasattr(self.__dict__['dist'], attr):
+                return getattr(self.__dict__['dist'], attr)
+            return getattr(self.__dict__['dist']._dist, attr)
+        return getattr(self.__dict__['dist'], attr)
+
+
+def get_installed_distributions(local_only=True, skip=None,
+                                include_editables=True, editables_only=False,
+                                user_only=False, use_cmd=False):
+    """
+    Directs call to function *get_installed_distributions* from :epkg:`pip`.
+
+    Return a list of installed Distribution objects.
+
+    :param local_only: if True (default), only return installations
+        local to the current virtualenv, if in a virtualenv.
+    :param skip: argument is an iterable of lower-case project names to
+        ignore; defaults to ``pip.compat.stdlib_pkgs`` (if *skip* is None)
+    :param editables: if False, don't report editables.
+    :param editables_only: if True , only report editables.
+    :param user_only: if True , only report installations in the user
+        site directory.
+    :param use_cmd: if True, use a different process (updated package list)
+    :return: list of installed Distribution objects.
+
+    .. versionadded:: 1.5
+    """
+    if use_cmd:
+        raise NotImplementedError("use_cmd should be False")
+    if skip is None:
+        try:
+            from pip._internal.utils.compat import stdlib_pkgs
+            skip = stdlib_pkgs
+        except ImportError:
+            pass
+    try:
+        from pip._internal.metadata import get_default_environment
+        return list(map(Distribution,
+                        get_default_environment().iter_installed_distributions(
+                            local_only=local_only, skip=skip,
+                            include_editables=include_editables,
+                            editables_only=editables_only,
+                            user_only=user_only)))
+
+    except ImportError:
+        from pip._internal.utils.misc import get_installed_distributions as getd
+        return list(map(Distribution, getd(
+            local_only=local_only, skip=skip,
+            include_editables=include_editables,
+            editables_only=editables_only,
+            user_only=user_only, use_cmd=use_cmd)))
+
+
 def get_packages_list():
     """
     calls ``pip list`` to retrieve the list of packages
     """
-    from pip._internal.utils.misc import get_installed_distributions
     return get_installed_distributions(local_only=True)
 
 
