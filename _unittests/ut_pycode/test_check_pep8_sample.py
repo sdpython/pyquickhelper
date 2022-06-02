7f97975ffdc55f7287fd3c2ec3a8a4e7488c29ca
Xavier Dupré <xadupre@users.noreply.github.com>
2022-06-02 17:28:59 +0200

    Fixes class SqlLite3FileStore where multiple thread open a sqlite3 co… (#368)
    * Fixes class SqlLite3FileStore where multiple thread open a sqlite3 connexion
    
    * remove useless code
    
    * Update test_builders_missing.py
    
    * Update azure-pipelines.yml
    
    * update CI
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update requirements.txt
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update requirements.txt
    
    * lint
    
    * 3.6

--- _unittests/ut_pycode/test_check_pep8_sample.py
+++ _unittests/ut_pycode/test_check_pep8_sample.py
@@ -3,8 +3,7 @@
 """
 import os
 import unittest
-
-from pyquickhelper.loghelper import fLOG
+import sys
 from pyquickhelper.pycode import check_pep8, ExtTestCase
 from pyquickhelper.pycode.utils_tests_helper import PEP8Exception
 
@@ -23,21 +22,18 @@ class TestCheckPep8Sample(ExtTestCase):
     def undeclared_var(self):
         aa + 5  # pylint: disable=E0602, W0104
 
+    @unittest.skipIf(sys.version_info[:2] <= (3, 6),
+                     reason="pylint not available in the last version")
     def test_unused_variable(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-
         this = os.path.abspath(os.path.dirname(__file__))
 
         def check_pep8_one_file():
-            check_pep8(this, fLOG=fLOG, max_line_length=150, recursive=False,
+            check_pep8(this, max_line_length=150, recursive=False,
                        neg_pattern="##",
                        pattern="test_check_pep8_sample.py")
 
         def check_pep8_error_file():
-            check_pep8(this, fLOG=fLOG, recursive=False,
+            check_pep8(this, recursive=False,
                        pylint_ignore=('C0111', 'R0201', 'C0103'),
                        pattern="test_check_pep8_sample.py",
                        neg_pattern="##",
@@ -49,12 +45,12 @@ class TestCheckPep8Sample(ExtTestCase):
         self.assertRaise(check_pep8_one_file, PEP8Exception,
                          "F[ECL1] line too long (link) 169 > 150")
 
-        check_pep8(this, fLOG=fLOG, max_line_length=170, recursive=False,
-                   pylint_ignore=('C0111', 'R0201', 'C0103'),
+        check_pep8(this, max_line_length=170, recursive=False,
+                   pylint_ignore=('C0111', 'C0103'),
                    pattern="test_check_pep8_sample.py",
                    neg_pattern="##",
                    skip=["test_check_pep8_sample.py:373: [E731]",
-                         "test_check_pep8_sample.py:36",
+                         "test_check_pep8_sample.py:35",
                          "test_check_pep8_sample.py:39",
                          "test_check_pep8_sample.py:11",
                          "test_check_pep8_sample.py:40: E0602",
